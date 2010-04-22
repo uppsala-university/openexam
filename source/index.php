f273f6eeef6dd7a5f2afb58b745d107577f6db7d
Anders Lövgren <anders.lovgren@bmc.uu.se>
2010-04-22 08:40:52 +0000

    Initial release
    git-svn-id: svn+ssh://svn.bmc.uu.se/cbe/etenta/trunk/openexam-php@659 58b8aac4-d633-40de-91bf-eab09b0297ab

---
+++ source/index.php
@@ -0,0 +1,113 @@
+<?php
+
+// 
+// Copyright (C) 2010 Computing Department BMC, 
+// Uppsala Biomedical Centre, Uppsala University.
+// 
+// File:   source/index.php
+// Author: Anders Lövgren
+// Date:   2010-04-21
+// 
+// The main entry point for the web application.
+// 
+
+// 
+// System check:
+// 
+if(!file_exists("../conf/database.conf")) {
+    header("location: admin/setup.php?reason=database");
+}
+if(!file_exists("../conf/config.inc")) {
+    header("location: admin/setup.php?reason=config");
+}
+
+// 
+// If logon is true, then CAS logon is enforced for this page.
+// 
+// $GLOBALS['logon'] = true;
+
+// 
+// Include external libraries:
+// 
+include "MDB2.php";
+include "CAS.php";
+
+// 
+// Include configuration:
+// 
+include "conf/config.inc";
+include "conf/database.conf";
+
+// 
+// Include logon, user interface and support for error reporting:
+// 
+include "include/cas.inc";
+include "include/ui.inc";
+include "include/error.inc";
+
+// 
+// Include database support:
+// 
+include "include/database.inc";
+
+// 
+// Locale and internationalization support:
+// 
+include "include/locale.inc";
+
+// 
+// This class implements a basic page.
+// 
+class IndexPage extends BasePage
+{
+    // 
+    // All possible request parameters should be added here along with
+    // the regex pattern to validate its value against.
+    // 
+    private $params = array();
+    
+    // 
+    // Construct the template page.
+    // 
+    public function __construct()
+    {	
+	parent::__construct(_("Index:"));   // Internationalized with GNU gettext
+    }
+
+    // 
+    // The template page body.
+    // 
+    public function printBody()
+    {
+	printf("<h3>" . _("Welcome!") . "</h3>\n");
+	printf("<p>" . _("This system let you do examination online, see <a href=\"%s\">Help</a> for more information.") . "</p>\n", "help.php");
+	printf("<h5>" . _("Are you ready to begin the examination?") . "</h5\n");
+	printf("<p>" . _("Follow the link to the <a href=\"%s\">examination page</a> to begin the examination. You will be prompted to logon using your UU-ID (CAS logon).") . "</p>\n", "exam.php");
+    }
+    
+    // 
+    // Validates request parameters.
+    // 
+    public function validate()
+    {
+	foreach($this->params as $param => $pattern) {
+	    if(isset($_REQUEST[$param])) {
+		if(!preg_match($pattern, $_REQUEST[$param])) {
+		    ErrorPage::show(_("Request parameter error!"),
+				    sprintf(_("Invalid value for request parameter '%s' (expected a value matching pattern '%s')."),
+					    $param, $pattern));
+		    exit(1);
+		}
+	    }
+	}
+    }
+}
+
+// 
+// Validate request parameters and (if validate succeeds) render the page.
+// 
+$page = new IndexPage();
+$page->validate();
+$page->render();
+
+?>
---
+++ template/page.php
@@ -0,0 +1,116 @@
+<?php
+
+// 
+// Copyright (C) 2009 Computing Department BMC, 
+// Uppsala Biomedical Centre, Uppsala University.
+// 
+// File:   template/page.php
+// Author: Anders Lövgren
+// Date:   2009-06-25
+// 
+// This is template script does the following:
+// 
+//   1. Check that the system is configured.
+//   2. Authenticate the user (optional).
+//   3. Validate request parameters.
+// 
+// The database connection has lazy initialization. Just call 
+// Database::getConnection() to start use it.
+// 
+
+// 
+// System check:
+// 
+if(!file_exists("../conf/database.conf")) {
+    header("location: admin/setup.php?reason=database");
+}
+if(!file_exists("../conf/config.inc")) {
+    header("location: admin/setup.php?reason=config");
+}
+
+// 
+// If logon is true, then CAS logon is enforced for this page.
+// 
+$GLOBALS['logon'] = true;
+
+// 
+// Include external libraries:
+// 
+include "MDB2.php";
+include "CAS.php";
+
+// 
+// Include configuration:
+// 
+include "conf/config.inc";
+include "conf/database.conf";
+
+// 
+// Include logon, user interface and support for error reporting:
+// 
+include "include/cas.inc";
+include "include/ui.inc";
+include "include/error.inc";
+
+// 
+// Include database support:
+// 
+include "include/database.inc";
+
+// 
+// Locale and internationalization support:
+// 
+include "include/locale.inc";
+
+// 
+// This class implements a basic page.
+// 
+class TemplatePage extends BasePage
+{
+    // 
+    // All possible request parameters should be added here along with
+    // the regex pattern to validate its value against.
+    // 
+    private $params = array( "action" => "/^(add|edit|delete)$/" );
+    
+    // 
+    // Construct the template page.
+    // 
+    public function __construct()
+    {	
+	parent::__construct(_("Template:"));   // Internationalized with GNU gettext
+    }
+
+    // 
+    // The template page body.
+    // 
+    public function printBody()
+    {
+    }
+    
+    // 
+    // Validates request parameters.
+    // 
+    public function validate()
+    {
+	foreach($this->params as $param => $pattern) {
+	    if(isset($_REQUEST[$param])) {
+		if(!preg_match($pattern, $_REQUEST[$param])) {
+		    ErrorPage::show(_("Request parameter error!"),
+				    sprintf(_("Invalid value for request parameter '%s' (expected a value matching pattern '%s')."),
+					    $param, $pattern));
+		    exit(1);
+		}
+	    }
+	}
+    }
+}
+
+// 
+// Validate request parameters and (if validate succeeds) render the page.
+// 
+$page = new TemplatePage();
+$page->validate();
+$page->render();
+
+?>
