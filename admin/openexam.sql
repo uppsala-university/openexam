7256ec78ac8923afea25cea1d8b972ca2a794f2d
Anders Lövgren <anders.lovgren@bmc.uu.se>
2010-04-22 17:57:41 +0000

    Saved SQL database schema (from MySQL).
    git-svn-id: svn+ssh://svn.bmc.uu.se/cbe/etenta/trunk/openexam-php@673 58b8aac4-d633-40de-91bf-eab09b0297ab

---
+++ admin/openexam.sql
@@ -0,0 +1,191 @@
+-- MySQL dump 10.11
+--
+-- Host: localhost    Database: openexam
+-- ------------------------------------------------------
+-- Server version	5.0.70-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `admins`
+--
+
+DROP TABLE IF EXISTS `admins`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `admins` (
+  `user` varchar(10) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Table structure for table `answers`
+--
+
+DROP TABLE IF EXISTS `answers`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `answers` (
+  `id` int(11) NOT NULL auto_increment,
+  `question_id` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `answer` text NOT NULL,
+  `comment` text,
+  PRIMARY KEY  (`id`),
+  KEY `question_id` (`question_id`),
+  KEY `student_id` (`student_id`),
+  CONSTRAINT `answers_ibfk_1` FOREIGN KEY (`question_id`) REFERENCES `questions` (`id`),
+  CONSTRAINT `answers_ibfk_2` FOREIGN KEY (`student_id`) REFERENCES `students` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Table structure for table `contributors`
+--
+
+DROP TABLE IF EXISTS `contributors`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `contributors` (
+  `id` int(11) NOT NULL auto_increment,
+  `exam_id` int(11) NOT NULL,
+  `user` char(8) NOT NULL,
+  PRIMARY KEY  (`id`),
+  KEY `exam_id` (`exam_id`),
+  CONSTRAINT `contributors_ibfk_1` FOREIGN KEY (`exam_id`) REFERENCES `exams` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Table structure for table `decoders`
+--
+
+DROP TABLE IF EXISTS `decoders`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `decoders` (
+  `id` int(11) NOT NULL auto_increment,
+  `exam_id` int(11) NOT NULL,
+  `user` char(8) NOT NULL,
+  PRIMARY KEY  (`id`),
+  KEY `exam_id` (`exam_id`),
+  CONSTRAINT `decoders_ibfk_1` FOREIGN KEY (`exam_id`) REFERENCES `exams` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Table structure for table `examinators`
+--
+
+DROP TABLE IF EXISTS `examinators`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `examinators` (
+  `id` int(11) NOT NULL auto_increment,
+  `exam_id` int(11) NOT NULL,
+  `user` char(8) NOT NULL,
+  PRIMARY KEY  (`id`),
+  KEY `exam_id` (`exam_id`),
+  CONSTRAINT `examinators_ibfk_1` FOREIGN KEY (`exam_id`) REFERENCES `exams` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Table structure for table `exams`
+--
+
+DROP TABLE IF EXISTS `exams`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `exams` (
+  `id` int(11) NOT NULL auto_increment,
+  `name` varchar(100) NOT NULL,
+  `descr` text,
+  `starttime` datetime NOT NULL,
+  `endtime` datetime NOT NULL,
+  `created` datetime NOT NULL,
+  `updated` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+  `creator` char(8) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Table structure for table `questions`
+--
+
+DROP TABLE IF EXISTS `questions`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `questions` (
+  `id` int(11) NOT NULL auto_increment,
+  `exam_id` int(11) NOT NULL,
+  `score` float NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `quest` text NOT NULL,
+  `user` char(8) NOT NULL,
+  `video` varchar(512) default NULL,
+  `image` varchar(512) default NULL,
+  `audio` varchar(512) default NULL,
+  `type` enum('freetext','single','multiple') NOT NULL default 'freetext',
+  PRIMARY KEY  (`id`),
+  KEY `exam_id` (`exam_id`),
+  CONSTRAINT `questions_ibfk_1` FOREIGN KEY (`exam_id`) REFERENCES `exams` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Table structure for table `results`
+--
+
+DROP TABLE IF EXISTS `results`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `results` (
+  `id` int(11) NOT NULL auto_increment,
+  `answer_id` int(11) NOT NULL,
+  `score` float NOT NULL,
+  `comment` text,
+  PRIMARY KEY  (`id`),
+  KEY `answer_id` (`answer_id`),
+  CONSTRAINT `results_ibfk_1` FOREIGN KEY (`answer_id`) REFERENCES `answers` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Table structure for table `students`
+--
+
+DROP TABLE IF EXISTS `students`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `students` (
+  `id` int(11) NOT NULL auto_increment,
+  `exam_id` int(11) NOT NULL,
+  `user` char(8) NOT NULL,
+  `code` varchar(15) NOT NULL,
+  PRIMARY KEY  (`id`),
+  KEY `exam_id` (`exam_id`),
+  CONSTRAINT `students_ibfk_1` FOREIGN KEY (`exam_id`) REFERENCES `exams` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8;
+SET character_set_client = @saved_cs_client;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2010-04-22 17:55:46
