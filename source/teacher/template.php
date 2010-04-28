5965dc333e523c8a1b4b26623faf67096a14377f
Anders Lövgren <anders.lovgren@bmc.uu.se>
2010-04-28 09:39:50 +0000

    Adding teacher page template.
    git-svn-id: svn+ssh://svn.bmc.uu.se/cbe/etenta/trunk/openexam-php@681 58b8aac4-d633-40de-91bf-eab09b0297ab

---
+++ source/teacher/template.php
@@ -0,0 +1,112 @@
+<?php
+
+// 
+// Copyright (C) 2010 Computing Department BMC, 
+// Uppsala Biomedical Centre, Uppsala University.
+// 
+// File:   source/teacher/template.php
+// Author: Anders Lövgren
+// Date:   2010-04-26
+// 
+
+// 
+// Force logon for unauthenticated users:
+// 
+$GLOBALS['logon'] = true;
+
+// 
+// System check:
+// 
+if(!file_exists("../../conf/database.conf")) {
+    header("location: setup.php?reason=database");
+}
+if(!file_exists("../../conf/config.inc")) {
+    header("location: setup.php?reason=config");
+}
+
+// 
+// Include external libraries:
+// 
+include "MDB2.php";
+include "CAS.php";
+
+// 
+// Locale and internationalization support:
+// 
+include "include/locale.inc";
+
+// 
+// Include configuration:
+// 
+include "conf/config.inc";
+include "conf/database.conf";
+
+// 
+// Include logon and user interface support:
+// 
+include "include/cas.inc";
+include "include/ui.inc";
+include "include/error.inc";
+
+// 
+// Include database support:
+// 
+include "include/database.inc";
+
+// 
+// Business logic:
+// 
+include "include/teacher.inc";
+
+// 
+// The template page:
+// 
+class TemplatePage extends TeacherPage
+{
+    private $params = array( "exam" => "/^\d+$/" );
+    
+    public function __construct()
+    {
+	parent::__construct(_("Template Page"), $this->params);
+    }
+
+    // 
+    // The main entry point. This is where all processing begins.
+    // 
+    public function printBody()
+    {
+        //
+	// Authorization first:
+	//
+	if(isset($_REQUEST['exam'])) {
+	    self::checkAccess($_REQUEST['exam']);
+	}
+	
+	//
+	// Bussiness logic:
+	//
+	if(isset($_REQUEST['exam'])) {
+	    die("TODO: implement bussiness logic");
+	} 
+    }
+
+    // 
+    // Verify that the caller has been granted the required role on this exam.
+    // 
+    private function checkAccess($exam)
+    {
+	$role = "role";   // TODO: replace!!
+	
+	if(!Teacher::userHasRole($exam, $role, phpCAS::getUser())) {
+	    ErrorPage::show(_("Access denied!"),
+			    sprintf(_("Only users granted the %s role on this exam can access this page. The script processing has halted."), $role));
+	    exit(1);
+	}
+    }
+        
+}
+
+$page = new TemplatePage();
+$page->render();
+
+?>
