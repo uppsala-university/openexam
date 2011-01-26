8f279153c25f7dc956cd1605ac87b21c40730458
Anders Lövgren <anders.lovgren@bmc.uu.se>
2011-01-26 21:41:18 +0000

    Added media library support.
    git-svn-id: svn+ssh://svn.bmc.uu.se/cbe/etenta/trunk/openexam-php@495 58b8aac4-d633-40de-91bf-eab09b0297ab

--- source/js/openexam.js
+++ source/js/openexam.js
@@ -19,6 +19,30 @@ function toggle_visibility(id) {
         }
 }
 
+//
+// Move object to current mouse position.
+//
+function move_object(obj, event) {
+        var posx = 0;
+        var posy = 0;
+        var e = event || window.event;
+
+        if (e.pageX || e.pageY) {
+                posx = e.pageX;
+                posy = e.pageY;
+        } else if (e.clientX || e.clientY) {
+                posx = e.clientX + document.body.scrollLeft
+                + document.documentElement.scrollLeft;
+                posy = e.clientY + document.body.scrollTop
+                + document.documentElement.scrollTop;
+        }
+        //
+        // posx and posy contains the mouse position relative to the document.
+        // 
+        obj.style.left = posx + 'px';
+        obj.style.top  = posy + 'px';
+}
+
 // 
 // This function starts an autosave of the form.
 // 
