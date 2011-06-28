bc025384663e959c5968d8da7e9cf31aa9c23555
Anders Lövgren <anders.lovgren@bmc.uu.se>
2011-06-28 16:13:10 +0000

    Set permission on media librray directories.
    git-svn-id: svn+ssh://svn.bmc.uu.se/cbe/etenta/trunk@516 58b8aac4-d633-40de-91bf-eab09b0297ab

--- openexam-php/admin/setperm.sh
+++ openexam-php/admin/setperm.sh
@@ -6,6 +6,19 @@
 # Date:   2010-02-22
 
 confdir=conf
+mediadir=source/media
 
 chmod 640 $confdir/config.inc $confdir/database.conf
 setfacl -m u:apache:r $confdir/config.inc $confdir/database.conf
+
+if [ -d $mediadir/files ]; then
+  # Must be writable when using automatic detection of file type.
+  setfacl -m u:apache:rwx $mediadir/files
+  for d in audio image video; do
+    if [ -d $mediadir/files/$d ]; then
+      setfacl -m u:apache:rwx $mediadir/files/$d
+    else
+      echo "$mediadir/files/$d is not a directory"
+    fi
+  done
+fi
