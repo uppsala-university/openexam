6aa0cdc96b0b331dded05a940bb50634bb35aff1
Anders Lövgren <anders.lovgren@bmc.uu.se>
2014-02-24 05:55:20 +0000

    Added simple Phalcon application, just an empty app without any functionality.
    git-svn-id: svn+ssh://svn.bmc.uu.se/cbe/openexam-php/branches/phalcon-mvc@1323 58b8aac4-d633-40de-91bf-eab09b0297ab

---
+++ openexam/app/config/config.php
@@ -0,0 +1,22 @@
+<?php
+
+$dbconf = new \Phalcon\Config\Adapter\Ini(__DIR__ . '/../../../config/database.ini');
+
+return new \Phalcon\Config(array(
+        'database'    => array(
+                'adapter'  => $dbconf->dbwrite->adapter,
+                'host'     => $dbconf->dbwrite->host,
+                'username' => $dbconf->dbwrite->username,
+                'password' => $dbconf->dbwrite->password,
+                'dbname'   => $dbconf->dbwrite->dbname,
+        ),
+        'application' => array(
+                'controllersDir' => __DIR__ . '/../../app/controllers/',
+                'modelsDir'      => __DIR__ . '/../../app/models/',
+                'viewsDir'       => __DIR__ . '/../../app/views/',
+                'pluginsDir'     => __DIR__ . '/../../app/plugins/',
+                'libraryDir'     => __DIR__ . '/../../app/library/',
+                'cacheDir'       => __DIR__ . '/../../app/cache/',
+                'baseUri'        => '/openexam/',
+        )
+    ));
