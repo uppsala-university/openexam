ab749d986350a6b5fbf89008bb1dd1a8b4263f04
Anders Lövgren <anders.lovgren@bmc.uu.se>
2014-02-24 17:05:22 +0000

    Create some handy defines.
    git-svn-id: svn+ssh://svn.bmc.uu.se/cbe/openexam-php/branches/phalcon-mvc@1333 58b8aac4-d633-40de-91bf-eab09b0297ab

--- openexam/app/config/config.php
+++ openexam/app/config/config.php
@@ -1,6 +1,6 @@
 <?php
 
-$config1 = new \Phalcon\Config\Adapter\Ini(__DIR__ . '/../../config/database.ini');
+$config1 = new \Phalcon\Config\Adapter\Ini(BASE_DIR . '/config/database.ini');
 $config2 = new \Phalcon\Config(array(
         'database'    => array(
                 'adapter'  => $config1->dbwrite->adapter,
@@ -10,12 +10,12 @@ $config2 = new \Phalcon\Config(array(
                 'dbname'   => $config1->dbwrite->dbname,
         ),
         'application' => array(
-                'controllersDir' => __DIR__ . '/../../app/controllers/',
-                'modelsDir'      => __DIR__ . '/../../app/models/',
-                'viewsDir'       => __DIR__ . '/../../app/views/',
-                'pluginsDir'     => __DIR__ . '/../../app/plugins/',
-                'libraryDir'     => __DIR__ . '/../../app/library/',
-                'cacheDir'       => __DIR__ . '/../../app/cache/',
+                'controllersDir' => APP_DIR . '/controllers/',
+                'modelsDir'      => APP_DIR . '/models/',
+                'viewsDir'       => APP_DIR . '/views/',
+                'pluginsDir'     => APP_DIR . '/plugins/',
+                'libraryDir'     => APP_DIR . '/library/',
+                'cacheDir'       => APP_DIR . '/cache/',
                 'baseUri'        => '/openexam/',
         )
     ));
