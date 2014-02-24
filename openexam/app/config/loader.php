2d588cb9dd459e89a6109141abf241f2882e6bc3
Anders Lövgren <anders.lovgren@bmc.uu.se>
2014-02-24 17:11:11 +0000

    Map application namespaces to locations.
    git-svn-id: svn+ssh://svn.bmc.uu.se/cbe/openexam-php/branches/phalcon-mvc@1337 58b8aac4-d633-40de-91bf-eab09b0297ab

--- openexam/app/config/loader.php
+++ openexam/app/config/loader.php
@@ -2,19 +2,11 @@
 
 $loader = new \Phalcon\Loader();
 
-/**
- * We're a registering a set of directories taken from the configuration file
- */
-$loader->registerDirs(
-    array(
-            $config->application->controllersDir,
-            $config->application->modelsDir
-    )
-);
-
 $loader->registerNamespaces(
     array(
-            'OpenExam' => $config->application->libraryDir
+            'OpenExam\Controllers' => $config->application->controllersDir,
+            'OpenExam\Models'      => $config->application->modelsDir,
+            'OpenExam'             => $config->application->libraryDir
     )
 );
 $loader->register();
