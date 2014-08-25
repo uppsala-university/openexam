eee9baabdbabc07f1f6b2535bd81b3bc2f77cfb8
Anders Lövgren <anders.lovgren@bmc.uu.se>
2014-08-25 07:25:45 +0000

    Adding core handler library. Still need to authorize remote caller based on roles (fetched from database) and access rules (filtering).
    git-svn-id: svn+ssh://svn.bmc.uu.se/cbe/openexam-php/branches/phalcon-mvc@1485 58b8aac4-d633-40de-91bf-eab09b0297ab

---
+++ openexam-with-handler/app/library/Core/Handler/TeacherHandler.php
@@ -0,0 +1,24 @@
+<?php
+
+// 
+// The source code is copyrighted, with equal shared rights, between the
+// authors (see the file AUTHORS) and the OpenExam project, Uppsala University 
+// unless otherwise explicit stated elsewhere.
+// 
+// File:    TeacherHandler.php
+// Created: 2014-08-25 02:29:58
+// 
+// Author:  Anders Lövgren (QNET/BMC CompDept)
+// 
+
+namespace OpenExam\Library\Core\Handler;
+
+/**
+ * Handler for Teacher model.
+ *
+ * @author Anders Lövgren (QNET/BMC CompDept)
+ */
+class TeacherHandler extends HandlerBase
+{
+
+}
