c3b6e43a59c77f3caafbfd9a1c4fd8b9e806b916
Anders Lövgren <anders.lovgren@bmc.uu.se>
2014-08-25 07:33:35 +0000

    Work in progress. This will be fundamental different.
    git-svn-id: svn+ssh://svn.bmc.uu.se/cbe/openexam-php/branches/phalcon-mvc@1490 58b8aac4-d633-40de-91bf-eab09b0297ab

---
+++ openexam-with-handler/app/library/Soap/BaseWsdlService.php
@@ -0,0 +1,25 @@
+<?php
+
+// 
+// The source code is copyrighted, with equal shared rights, between the
+// authors (see the file AUTHORS) and the OpenExam project, Uppsala University 
+// unless otherwise explicit stated elsewhere.
+// 
+// File:    BaseWsdlService.php
+// Created: 2014-08-21 01:14:41
+// 
+// Author:  Anders Lövgren (QNET/BMC CompDept)
+// 
+
+namespace OpenExam\Library\Core;
+
+/**
+ * Description of BaseWsdlService
+ *
+ * @author Anders Lövgren (QNET/BMC CompDept)
+ */
+class BaseWsdlService
+{
+
+        //put your code here
+}
