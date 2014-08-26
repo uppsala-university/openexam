4d0fa9a0be33c7b9a0d147bd88c02d47b9dc526d
Anders Lövgren <anders.lovgren@bmc.uu.se>
2014-08-26 15:00:03 +0000

    Implemented core REST service controller.
    git-svn-id: svn+ssh://svn.bmc.uu.se/cbe/openexam-php/branches/phalcon-mvc@1494 58b8aac4-d633-40de-91bf-eab09b0297ab

--- openexam-with-handler/app/controllers/Core/RestController.php
+++ openexam-with-handler/app/controllers/Core/RestController.php
@@ -14,12 +14,137 @@
 namespace OpenExam\Controllers\Core;
 
 /**
- * Description of RestController
+ * REST request helper class.
+ */
+class RestRequest
+{
+
+        public $target;
+        public $method;
+        public $action;
+        public $model;
+        public $role;
+        public $data = array();
+
+        public function __construct($method, $params)
+        {
+                if (isset($params['role'])) {
+                        $this->role = $params['role'];
+                        unset($params['role']);
+                }
+                if (isset($params['target'])) {
+                        $this->target = $params['target'];
+                        unset($params['target']);
+                }
+
+                $this->method = $method;
+                $this->action = $this->getAction();
+
+                if ($this->method == 'POST' || $this->method == 'PUT') {
+                        $this->setData();
+                }
+
+                array_unshift($params, $this->target);
+                $params = array_reverse($params);
+
+                // 
+                // Map params onto model, primary and foreign keys:
+                // 
+                if (is_numeric($params[0])) {
+                        $this->model = substr($params[1], 0, -1);
+                        $this->data['id'] = $params[0];
+                } else {
+                        $this->model = substr($params[0], 0, -1);
+                        if (isset($params[2])) {
+                                $foreign = sprintf("%s_id", substr($params[2], 0, -1));
+                                $this->data[$foreign] = $params[1];
+                        }
+                }
+        }
+
+        private function getAction()
+        {
+                switch ($this->method) {
+                        case 'GET':
+                                return 'read';
+                        case 'POST':
+                                return 'create';
+                        case 'PUT':
+                                return 'update';
+                        case 'DELETE':
+                                return 'delete';
+                }
+        }
+
+        private function setData()
+        {
+                switch ($this->method) {
+                        case 'POST':
+                                $this->data = $_POST;
+                                break;
+                        case 'PUT':
+                                parse_str(file_get_contents("php://input"), $this->data);
+                                break;
+                }
+        }
+
+}
+
+/**
+ * REST controller for core service.
  *
  * @author Anders Lövgren (QNET/BMC CompDept)
  */
 class RestController extends \OpenExam\Controllers\ServiceController
 {
 
-        //put your code here
+        public function initialize()
+        {
+                parent::initialize();
+        }
+
+        public function apiAction()
+        {
+                $content = array(
+                        "usage"   => array(
+                                "/core/rest/{role}/{target}"      => array("GET", "POST"),
+                                "/core/rest/{role}/{target}/{id}" => array("PUT", "DELETE"),
+                                "/core/rest/{role}/exam/search"   => "POST"
+                        ),
+                        "example" => array(
+                                "/core/rest/student/exams/44/questions/22/answers",
+                                "/core/rest/student/exams/44/questions/22/answers/123",
+                                "/core/rest/student/admins",
+                                "/core/rest/student/teachers",
+                                "/core/rest/student/rooms"
+                        )
+                );
+
+                $this->response->setJsonContent($content);
+                $this->response->send();
+        }
+
+        public function indexAction()
+        {
+                $request = $this->getRestRequest();
+
+                $hobj = self::createHandler($request->role, $request->model);
+                $mobj = self::createModel($request->model, $request->data);
+
+                $this->response->setJsonContent($hobj->$action($mobj));
+                $this->response->send();
+        }
+
+        public function searchAction()
+        {
+                
+        }
+
+        private function getRestRequest()
+        {
+                return new RestRequest(
+                    $this->request->getMethod(), $this->dispatcher->getParams()
+                );
+        }
+
 }
