77103a592521cb28e3ea5b0f0a37297570677504
Anders Lövgren <anders.lovgren@bmc.uu.se>
2014-08-27 10:15:29 +0000

    Adding support for system wide and object specific roles. Using parameter binding in queries against model.
    git-svn-id: svn+ssh://svn.bmc.uu.se/cbe/openexam-php/branches/phalcon-mvc@1500 58b8aac4-d633-40de-91bf-eab09b0297ab

--- openexam-with-handler/app/library/Core/Security/Roles.php
+++ openexam-with-handler/app/library/Core/Security/Roles.php
@@ -18,10 +18,36 @@ use OpenExam\Models\Admin,
     OpenExam\Models\Decoder,
     OpenExam\Models\Invigilator,
     OpenExam\Models\Student,
-    OpenExam\Models\Teacher;
+    OpenExam\Models\Teacher,
+    OpenExam\Models\Question;
 
 /**
  * Manage roles for user session.
+ * 
+ * Roles are aquired either system wide or bound to an object (identified
+ * by the object ID in the model). Internal the aquired roles are stored 
+ * as:
+ * 
+ * $roles[0][role]      // System wide role.
+ * $roles[id][role]     // Object specific role.
+ * 
+ * For example, if $roles[0]['contributor'] means that the authenticated
+ * user (stored in session object) has the 'contributor' role on at least
+ * one exam.
+ * 
+ * Aquiring a role for an specific object will also automatic grant that user
+ * the same role system wide.
+ * 
+ * These are the defined roles along with their object specific binding:
+ * 
+ * - admin              : system wide
+ * - teacher            : system wide
+ * - creator            : object specific (bound to exam)
+ * - contributor        : object specific (bound to exam) 
+ * - invigilator        : object specific (bound to exam)
+ * - decoder            : object specific (bound to exam) 
+ * - student            : object specific (bound to exam) 
+ * - corrector          : object specific (bound to question)
  * 
  * @author Anders Lövgren (QNET/BMC CompDept)
  */
@@ -55,53 +81,65 @@ class Roles extends \Phalcon\Mvc\User\Plugin
 
         /**
          * Add role to collection.
-         * @param string $role
+         * @param string $role The role to add.
+         * @param int $id The object ID.
          */
-        public function addRole($role)
+        public function addRole($role, $id = 0)
         {
-                $this->roles[$role] = true;
+                if ($id == 0) {
+                        $this->roles[0][$role] = true;
+                } else {
+                        $this->roles[0][$role] = true;
+                        $this->roles[$id][$role] = true;
+                }
         }
 
         /**
          * Remove role from collection.
-         * @param string $role
+         * @param string $role The role to remove.
+         * @param int $id The object ID.
          */
-        public function removeRole($role)
+        public function removeRole($role, $id = 0)
         {
-                if (array_key_exists($role, $this->roles)) {
-                        unset($this->roles[$role]);
+                if (array_key_exists($role, $this->roles[$id])) {
+                        unset($this->roles[$id][$role]);
                 }
         }
 
         /**
          * Check if role exists.
-         * @param string $role
+         * @param string $role The role to check.
+         * @param int $id The object ID.
          * @return bool
          */
-        public function hasRole($role)
+        public function hasRole($role, $id = 0)
         {
-                return array_key_exists($role, $this->roles);
+                return array_key_exists($role, $this->roles[$id]);
         }
 
         /**
          * Get all roles in collection.
+         * @param int $id The object ID.
          * @return array
          */
-        public function getRoles()
+        public function getRoles($id = 0)
         {
-                return array_keys($this->roles);
+                return array_keys($this->roles[$id]);
         }
 
         /**
-         * Try to aquire role. Returns true if successful. The exam is
-         * is optional.
+         * Try to aquire role. Returns true if successful. The object ID is
+         * is optional. 
+         * 
+         * If the object ID is missing, then the role is aquired system wide. 
+         * If supplied, then requested role is aquired on that specific object. 
          * 
          * @param string $role The role name.
-         * @param int $exam The exam ID.
+         * @param int $id The object ID.
          */
-        public function aquire($role, $exam = 0)
+        public function aquire($role, $id = 0)
         {
-                if ($this->hasRole($role)) {
+                if ($this->hasRole($role, $id)) {
                         return true;
                 }
 
@@ -109,44 +147,124 @@ class Roles extends \Phalcon\Mvc\User\Plugin
                 $user = $this->session->get('auth')['name'];
 
                 if ($role == self::admin) {
-                        if (Admin::count("user='" . $user . "'")) {
+                        $parameters = array(
+                                "user = :user:",
+                                "bind" => array("user" => $user)
+                        );
+                        if (Admin::count($parameters)) {
                                 $this->addRole($role);
                                 return true;
                         }
                 }
                 if ($role == self::teacher) {
-                        if (Teacher::count("user='" . $user . "'")) {
+                        $parameters = array(
+                                "user = :user:",
+                                "bind" => array("user" => $user)
+                        );
+                        if (Teacher::count($parameters)) {
                                 $this->addRole($role);
                                 return true;
                         }
                 }
                 if ($role == self::contributor) {
-                        if (Contributor::count("user='" . $user . "' AND exam_id=" . $exam)) {
-                                $this->addRole($role);
+                        if ($id != 0) {
+                                $parameters = array(
+                                        "user = :user: AND exam_id = :id:",
+                                        "bind" => array("user" => $user, "id" => $id)
+                                );
+                        } else {
+                                $parameters = array(
+                                        "user = :user:",
+                                        "bind" => array("user" => $user)
+                                );
+                        }
+                        if (Contributor::count($parameters) > 0) {
+                                $this->addRole($role, $id);
                                 return true;
                         }
                 }
                 if ($role == self::decoder) {
-                        if (Decoder::count("user='" . $user . "' AND exam_id=" . $exam)) {
-                                $this->addRole($role);
+                        if ($id != 0) {
+                                $parameters = array(
+                                        "user = :user: AND exam_id = :id:",
+                                        "bind" => array("user" => $user, "id" => $id)
+                                );
+                        } else {
+                                $parameters = array(
+                                        "user = :user:",
+                                        "bind" => array("user" => $user)
+                                );
+                        }
+                        if (Decoder::count($parameters) > 0) {
+                                $this->addRole($role, $id);
                                 return true;
                         }
                 }
                 if ($role == self::invigilator) {
-                        if (Invigilator::count("user='" . $user . "' AND exam_id=" . $exam)) {
-                                $this->addRole($role);
+                        if ($id != 0) {
+                                $parameters = array(
+                                        "user = :user: AND exam_id = :id:",
+                                        "bind" => array("user" => $user, "id" => $id)
+                                );
+                        } else {
+                                $parameters = array(
+                                        "user = :user:",
+                                        "bind" => array("user" => $user)
+                                );
+                        }
+                        if (Invigilator::count($parameters) > 0) {
+                                $this->addRole($role, $id);
                                 return true;
                         }
                 }
                 if ($role == self::student) {
-                        if (Student::count("user='" . $user . "' AND exam_id=" . $exam)) {
-                                $this->addRole($role);
+                        if ($id != 0) {
+                                $parameters = array(
+                                        "user = :user: AND exam_id = :id:",
+                                        "bind" => array("user" => $user, "id" => $id)
+                                );
+                        } else {
+                                $parameters = array(
+                                        "user = :user:",
+                                        "bind" => array("user" => $user)
+                                );
+                        }
+                        if (Student::count($parameters) > 0) {
+                                $this->addRole($role, $id);
                                 return true;
                         }
                 }
                 if ($role == self::creator) {
-                        if (Exam::count("creator='" . $user . "' AND id=" . $exam)) {
-                                $this->addRole($role);
+                        if ($id != 0) {
+                                $parameters = array(
+                                        "creator = :user: AND id = :id:",
+                                        "bind" => array("user" => $user, "id" => $id)
+                                );
+                        } else {
+                                $parameters = array(
+                                        "creator = :user:",
+                                        "bind" => array("user" => $user)
+                                );
+                        }
+                        if (Exam::count($parameters) > 0) {
+                                $this->addRole($role, $id);
+                                return true;
+                        }
+                }
+                if ($role == self::corrector) {
+                        if ($id != 0) {
+                                $parameters = array(
+                                        "user = :user: AND id = :id:",
+                                        "bind" => array("user" => $user, "id" => $id)
+                                );
+                        } else {
+                                $parameters = array(
+                                        "creator = :user:",
+                                        "bind" => array("user" => $user)
+                                );
+                        }
+                        if (Question::count($parameters) > 0) {
+                                $this->addRole($role, $id);
                                 return true;
                         }
                 }
