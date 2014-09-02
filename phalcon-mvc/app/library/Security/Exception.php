c66dd9a16b548d079ab739f8d95a4d34715213a7
Anders Lövgren <anders.lovgren@bmc.uu.se>
2014-09-02 22:15:37 +0000

    Add Exception class for namespace OpenExam\Library\Security.
    git-svn-id: svn+ssh://svn.bmc.uu.se/cbe/openexam-php/branches/phalcon-mvc@1537 58b8aac4-d633-40de-91bf-eab09b0297ab

---
+++ phalcon-mvc/app/library/Security/Exception.php
@@ -0,0 +1,19 @@
+<?php
+
+// 
+// The source code is copyrighted, with equal shared rights, between the
+// authors (see the file AUTHORS) and the OpenExam project, Uppsala University 
+// unless otherwise explicit stated elsewhere.
+// 
+// File:    Exception.php
+// Created: 2014-09-02 23:15:25
+// 
+// Author:  Anders Lövgren (QNET/BMC CompDept)
+// 
+
+namespace OpenExam\Library\Security;
+
+class Exception extends \Exception
+{
+        
+}
