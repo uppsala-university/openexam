d6feb70c4ad6f6aa8fef25e9f759c696885db553
Anders Lövgren <anders.lovgren@bmc.uu.se>
2014-09-09 03:52:50 +0000

    Finalizing the database migration task. Adding phalcon-mvc/script/database.php as command line script.
    git-svn-id: svn+ssh://svn.bmc.uu.se/cbe/openexam-php/branches/phalcon-mvc@1601 58b8aac4-d633-40de-91bf-eab09b0297ab

--- phalcon-mvc/app/tasks/DatabaseTask.php
+++ phalcon-mvc/app/tasks/DatabaseTask.php
@@ -13,22 +13,85 @@
 
 namespace OpenExam\Console\Tasks;
 
+use OpenExam\Library\Database\Migration;
+
 /**
  * Database task.
  *
  * @author Anders Lövgren (Computing Department at BMC, Uppsala University)
  */
-class DatabaseTask extends \Phalcon\CLI\Task
+class DatabaseTask extends MainTask
 {
 
-        public function indexAction()
+        /**
+         * Display usage information.
+         */
+        public function helpAction()
         {
-                printf(__METHOD__ . PHP_EOL);
+                printf("Usage: --migrate [--version=x.y.z] [--force]\n");
+                printf("       --status\n");
+                printf("Options:\n");
+                printf("  --migrate:       Perform database migration.\n");
+                printf("  --version=x.y.z: Migrate database to given version.\n");
+                printf("  --force:         Force migration even if already applied.\n");
+                printf("  --status:        Display current migration status and exit.\n");
+                printf("  --verbose:       Be more verbose.\n");
         }
 
-        public function migrateAction()
+        /**
+         * Database migrate action.
+         * 
+         * @param array $params Optional parameters ('version', 'force', 'verbose').
+         */
+        public function migrateAction($params = array())
         {
-                printf(__METHOD__ . PHP_EOL);
+                $options = (object) array(
+                            'verbose' => false,
+                            'version' => null,
+                            'force'   => false
+                );
+
+                for ($i = 0; $i < count($params); ++$i) {
+                        if ($params[$i] == 'version') {
+                                $options->version = $params[++$i];
+                        }
+                        if ($params[$i] == 'force') {
+                                $options->force = true;
+                        }
+                        if ($params[$i] == 'verbose') {
+                                $options->verbose = true;
+                        }
+                }
+
+                if ($options->verbose) {
+                        printf("Starting database migration to version '%s':\n", isset($options->version) ? $options->version : 'latest');
+                        printf("-------------------------------------------------\n");
+                        printf("    Type: %s\n", $this->config->database->adapter);
+                        printf("    Name: %s\n", $this->config->database->dbname);
+                        printf("    User: %s\n", $this->config->database->username);
+                        printf("    Host: %s\n", $this->config->database->host);
+                        printf("\n");
+                }
+
+                Migration::run($this->config, $options->version, $options->force);
+        }
+
+        /**
+         * Migrate status action.
+         * @param array $params Optional parameters ('verbose').
+         */
+        public function statusAction($params = array())
+        {
+                $file = sprintf("%s/.phalcon/migration-version", $this->config->application->baseDir);
+
+                if (in_array('verbose', $params)) {
+                        printf("Checking for status info in %s\n", $file);
+                }
+                if (file_exists($file)) {
+                        printf("Current database migration version: %s\n", file_get_contents($file));
+                } else {
+                        printf("No database migration status found.\n");
+                }
         }
 
 }
