50efb17d0e145ed148d96ba56f0c9915e0eb7cd4
Anders Lövgren <anders.lovgren@bmc.uu.se>
2014-09-09 09:09:06 +0000

    Adding --generate action to database task. Updating database migration docs.
    git-svn-id: svn+ssh://svn.bmc.uu.se/cbe/openexam-php/branches/phalcon-mvc@1604 58b8aac4-d633-40de-91bf-eab09b0297ab

--- phalcon-mvc/app/tasks/DatabaseTask.php
+++ phalcon-mvc/app/tasks/DatabaseTask.php
@@ -30,41 +30,92 @@ class DatabaseTask extends MainTask
         {
                 printf("Usage: --migrate [--version=x.y.z] [--force]\n");
                 printf("       --status\n");
+                printf("\n");
                 printf("Options:\n");
-                printf("  --migrate:       Perform database migration.\n");
-                printf("  --version=x.y.z: Migrate database to given version.\n");
+                printf("  --migrate:       Perform database migration (use latest version by default).\n");
+                printf("  --generate:      Generate database migration (automatic version if not given).\n");
+                printf("  --version=x.y.z: Version of migration or generation task.\n");
                 printf("  --force:         Force migration even if already applied.\n");
                 printf("  --status:        Display current migration status and exit.\n");
                 printf("  --verbose:       Be more verbose.\n");
+                printf("\n");
+                printf("Examples:\n");
+                printf("  # Generate migration, overwrite if existing:\n");
+                printf("  --generate --verbose --version=2.0.0 --force\n");
+                printf("\n");
+                printf("  # Migrate database to latest:\n");
+                printf("  --migrate --verbose\n");
         }
 
         /**
          * Database migrate action.
-         * 
          * @param array $params Optional parameters ('version', 'force', 'verbose').
          */
         public function migrateAction($params = array())
         {
-                $options = (object) array(
-                            'verbose' => false,
-                            'version' => null,
-                            'force'   => false
+                $options = array(
+                        'verbose' => false,
+                        'version' => null,
+                        'force'   => false
                 );
 
                 for ($i = 0; $i < count($params); ++$i) {
                         if ($params[$i] == 'version') {
-                                $options->version = $params[++$i];
+                                $options['version'] = $params[++$i];
                         }
                         if ($params[$i] == 'force') {
-                                $options->force = true;
+                                $options['force'] = true;
                         }
                         if ($params[$i] == 'verbose') {
-                                $options->verbose = true;
+                                $options['verbose'] = true;
                         }
                 }
 
-                if ($options->verbose) {
-                        printf("Starting database migration to version '%s':\n", isset($options->version) ? $options->version : 'latest');
+                if ($options['verbose']) {
+                        printf("Starting database migration to version '%s':\n", isset($options['version']) ? $options['version'] : 'latest');
+                        printf("-------------------------------------------------\n");
+                        printf("    Type: %s\n", $this->config->database->adapter);
+                        printf("    Name: %s\n", $this->config->database->dbname);
+                        printf("    User: %s\n", $this->config->database->username);
+                        printf("    Host: %s\n", $this->config->database->host);
+                        printf("  Source: %s\n", $this->config->application->migrationsDir);
+                        printf("\n");
+                }
+
+                Migration::run($this->config, $options['version'], $options['force']);
+        }
+
+        /**
+         * Generate database migration.
+         * @param array $params Optional parameters ('version', 'verbose').
+         */
+        public function generateAction($params = array())
+        {
+                $options = array(
+                        'migrationsDir'   => $this->config->application->migrationsDir,
+                        'config'          => $this->config,
+                        'directory'       => $this->config->application->baseDir,
+                        'verbose'         => false,
+                        'originalVersion' => null,
+                        'force'           => false,
+                        'tableName'       => 'all',
+                        'exportData'      => null
+                );
+
+                for ($i = 0; $i < count($params); ++$i) {
+                        if ($params[$i] == 'version') {
+                                $options['originalVersion'] = $params[++$i];
+                        }
+                        if ($params[$i] == 'force') {
+                                $options['force'] = true;
+                        }
+                        if ($params[$i] == 'verbose') {
+                                $options['verbose'] = true;
+                        }
+                }
+
+                if ($options['verbose']) {
+                        printf("Generate database migration as version '%s':\n", isset($options['version']) ? $options['version'] : '<automatic>');
                         printf("-------------------------------------------------\n");
                         printf("    Type: %s\n", $this->config->database->adapter);
                         printf("    Name: %s\n", $this->config->database->dbname);
@@ -73,7 +124,7 @@ class DatabaseTask extends MainTask
                         printf("\n");
                 }
 
-                Migration::run($this->config, $options->version, $options->force);
+                \Phalcon\Migrations::generate($options);
         }
 
         /**
