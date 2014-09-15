36a88187c306ead0cb4fecdac742d038eb6f1629
Anders Lövgren <anders.lovgren@bmc.uu.se>
2014-09-15 01:27:25 +0000

    Adding Composer install/update hooks (using scripts declarations).
    git-svn-id: svn+ssh://svn.bmc.uu.se/cbe/openexam-php/branches/phalcon-mvc@1620 58b8aac4-d633-40de-91bf-eab09b0297ab

---
+++ admin/composer/Installer.php
@@ -0,0 +1,64 @@
+<?php
+
+// 
+// The source code is copyrighted, with equal shared rights, between the
+// authors (see the file AUTHORS) and the OpenExam project, Uppsala University 
+// unless otherwise explicit stated elsewhere.
+// 
+// File:    Composer.php
+// Created: 2014-09-13 00:39:37
+// 
+// Author:  Anders Lövgren (QNET/BMC CompDept)
+// 
+
+namespace OpenExam\Composer;
+
+use Composer\Script\Event;
+
+/**
+ * Handles install/update hooks for Composer.
+ * 
+ * @author Anders Lövgren (QNET/BMC CompDept)
+ */
+class Installer
+{
+
+        /**
+         * Package to installer class map.
+         * @var array 
+         */
+        private static $packages = array(
+                'phalcon/devtools' => 'PhalconInstaller'
+        );
+
+        /**
+         * Package install hook.
+         * @param \Composer\Script\Event $event
+         */
+        public static function postPackageInstall(Event $event)
+        {
+                $package = $event->getOperation()->getPackage();
+
+                if (array_key_exists($package->getName(), self::$packages)) {
+                        $handler = self::$packages[$package->getName()];
+                        $installer = new $handler($package);
+                        $installer->install();
+                }
+        }
+
+        /**
+         * Package update hook.
+         * @param \Composer\Script\Event $event
+         */
+        public static function postPackageUpdate(Event $event)
+        {
+                $package = $event->getOperation()->getPackage();
+
+                if (array_key_exists($package->getName(), self::$packages)) {
+                        $handler = self::$packages[$package->getName()];
+                        $installer = new $handler($package);
+                        $installer->update();
+                }
+        }
+
+}
---
+++ admin/composer/PackageInstaller.php
@@ -0,0 +1,143 @@
+<?php
+
+// 
+// The source code is copyrighted, with equal shared rights, between the
+// authors (see the file AUTHORS) and the OpenExam project, Uppsala University 
+// unless otherwise explicit stated elsewhere.
+// 
+// File:    PackageInstaller.php
+// Created: 2014-09-13 01:57:30
+// 
+// Author:  Anders Lövgren (QNET/BMC CompDept)
+// 
+
+namespace OpenExam\Composer;
+
+use Composer\Package\CompletePackageInterface;
+
+/**
+ * Composer package installer.
+ *
+ * @author Anders Lövgren (QNET/BMC CompDept)
+ */
+abstract class PackageInstaller
+{
+
+        /**
+         * Interface for the Composer package.
+         * @var \Composer\Package\CompletePackageInterface 
+         */
+        protected $package;
+        /**
+         * The package name.
+         * @var string 
+         */
+        protected $name;
+        /**
+         * The package version.
+         * @var string 
+         */
+        protected $version;
+        /**
+         * The package destination directory.
+         * @var string 
+         */
+        protected $destdir;
+
+        /**
+         * Constructor.
+         * @param \Composer\Package\CompletePackageInterface $package
+         */
+        public function __construct(CompletePackageInterface $package)
+        {
+                $this->package = $package;
+                $this->name = $package->getName();
+                $this->version = $package->getVersion();
+                $this->destdir = $package->getTargetDir();
+
+                if (empty($this->destdir)) {
+                        $this->destdir = sprintf("vendor/%s", $this->name);
+                }
+        }
+
+        /**
+         * Create symbolic link.
+         * 
+         * <code>
+         * $this->symlink("ide/1.3.2", "api");
+         * $this->symlink("docs", "docs/package", "vendor");
+         * </code>
+         * @param string $source The link source.
+         * @param string $target The link target.
+         * @param string $rootdir The link destination directory (default to $destdir).
+         * @return boolean
+         */
+        protected function symlink($source, $target, $rootdir = null)
+        {
+                $cwd = getcwd();
+
+                if (!isset($rootdir)) {
+                        $rootdir = $this->destdir;
+                }
+
+                if (!chdir($rootdir)) {
+                        fprintf(STDERR, "%s: Failed change working directory to %s\n", __METHOD__, $rootdir);
+                        return false;
+                }
+                if (!symlink($source, $target)) {
+                        fprintf(STDERR, "%s: Failed create symlink (%s -> %s)\n", __METHOD__, $source, $target);
+                        chdir($cwd);
+                        return false;
+                }
+
+                chdir($cwd);
+                return true;
+        }
+
+        /**
+         * Apply unified diff/patch.
+         * @param string $file The patch file.
+         * @param string $rootdir The directory to apply patch from (defaults to vendor).
+         * @return boolean
+         */
+        protected function patch($file, $rootdir = null)
+        {
+                $cwd = getcwd();
+
+                $command = sprintf("patch -p0 -fi %s", $file);
+                $output = "";
+                $status = 0;
+
+                if (isset($rootdir) && !chdir($rootdir)) {
+                        fprintf(STDERR, "%s: Failed change working directory to %s\n", __METHOD__, $rootdir);
+                        return false;
+                }
+
+                exec($command, $output, $status);
+                if ($status != 0) {
+                        fprintf(STDERR, "%s: Failed apply patch %s: %s\n", __METHOD__, basename($file), $output);
+                        chdir($cwd);
+                        return false;
+                }
+
+                chdir($cwd);
+                return true;
+        }
+
+        /**
+         * Package install event hook.
+         */
+        public function install()
+        {
+                
+        }
+
+        /**
+         * Package update event hook.
+         */
+        public function update()
+        {
+                
+        }
+
+}
