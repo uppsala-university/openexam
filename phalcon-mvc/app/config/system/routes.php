uppsala-university/openexam

commit 3226371a0296150fa6cb716ae4cf26dfc3a09ad3

changes in auth.def as per changed login wrappers
git-svn-id: svn+ssh://svn.bmc.uu.se/cbe/openexam-php/branches/phalcon-mvc@1614 58b8aac4-d633-40de-91bf-eab09b0297ab

phalcon-mvc/app/config/auth.def.in
@@ -11,9 +11,9 @@
 // Author:  Anders Lövgren (Computing Department at BMC, Uppsala University)
 // 
 
-use UUP\Authentication\Authenticator\CasAuthenticator,
-    UUP\Authentication\Authenticator\FormAuthenticator,
-    UUP\Authentication\Validator\LdapBindValidator;
+use UUP\Authentication\Validator\LdapBindValidator,
+    OpenExam\Library\Security\Authenticator\RemoteLogins\CasLogin,
+    OpenExam\Library\Security\Authenticator\Formlogins\MsadLogin;
 
 // 
 // The chain of authenticator plugins for various services:
@@ -33,15 +33,15 @@ return array(
         // Web/AJAX service authentication:
         // 
         'web'  => array(
-                'cas'  => (new CasAuthenticator(
-                    'cas.example.com', 443, '/cas'
-                    ))
-                    ->visible(true)
-                    ->description('CAS Login')
-                    ->name('cas'),
-                'msad' => (new FormAuthenticator(
-                    new LdapBindValidator('ldaps://ldap.example.com', 636), array(
-                        'login' => '/login/msad',
+                'cas'  => (new CasLogin(
+                        'cas.weblogin.uu.se', 443, '/cas'
+                        ))
+                        ->visible(true)
+                        ->description('CAS Login')
+                        ->name('cas'),
+                'msad' => (new MsadLogin(
+                    new LdapBindValidator('ldaps://akka-ldap.uu.se', 636), array(
+                        'login' => '/msad',
                         'name'  => 'auth')
                     ))
                     ->visible(true)

commit fb15a9b3cb983d46a2fc924ba7e2b0339afdf49c

Regression in r1614: All *.in under phalcon-mvc/app/config is config protected (template) files.
git-svn-id: svn+ssh://svn.bmc.uu.se/cbe/openexam-php/branches/phalcon-mvc@1643 58b8aac4-d633-40de-91bf-eab09b0297ab

phalcon-mvc/app/config/access.def.in
@@ -10,7 +10,6 @@
 // 
 // Author:  Anders Lövgren (Computing Department at BMC, Uppsala University)
 // 
-
 // 
 // The map between role, resource and permission. This map is used
 // for defining the ACL in the system.

phalcon-mvc/app/config/auth.def.in
@@ -33,15 +33,15 @@ return array(
         // Web/AJAX service authentication:
         // 
         'web'  => array(
-                'cas'  => (new CasLogin(
-                        'cas.weblogin.uu.se', 443, '/cas'
-                        ))
-                        ->visible(true)
-                        ->description('CAS Login')
-                        ->name('cas'),
-                'msad' => (new MsadLogin(
-                    new LdapBindValidator('ldaps://akka-ldap.uu.se', 636), array(
-                        'login' => '/msad',
+                'cas'  => (new CasAuthenticator(
+                    'cas.example.com', 443, '/cas'
+                    ))
+                    ->visible(true)
+                    ->description('CAS Login')
+                    ->name('cas'),
+                'msad' => (new FormAuthenticator(
+                    new LdapBindValidator('https://ldap.example.com', 636), array(
+                        'login' => '/login/msad',
                         'name'  => 'auth')
                     ))
                     ->visible(true)

phalcon-mvc/app/config/system/routes.php
@@ -87,13 +87,13 @@ $router->mount(
  */
 $router->add(
     "/auth/:action/?(.*)?", array(
-        "controller"    => "auth",
-        "action"        => 1,
-        "authMethod"    => 2,
-        "namespace"     => "OpenExam\Controllers\Gui"
+        "controller" => "auth",
+        "action"     => 1,
+        "authMethod" => 2,
+        "namespace"  => "OpenExam\Controllers\Gui"
     )
 );
- 
+
 
 /**
  * Route SOAP and WSDL requests: