44e51b6822e8b488e56c8894aac4160dd409afb0
Anders Lövgren <anders.lovgren@bmc.uu.se>
2014-10-02 02:26:30 +0000

    Implemented object level access control for answer model.
    git-svn-id: svn+ssh://svn.bmc.uu.se/cbe/openexam-php/branches/phalcon-mvc@1722 58b8aac4-d633-40de-91bf-eab09b0297ab

--- phalcon-mvc/app/plugins/Security/Model/AnswerAccess.php
+++ phalcon-mvc/app/plugins/Security/Model/AnswerAccess.php
@@ -13,6 +13,7 @@
 
 namespace OpenExam\Plugins\Security\Model;
 
+use OpenExam\Library\Security\Exception;
 use OpenExam\Library\Security\Roles;
 use OpenExam\Library\Security\User;
 use OpenExam\Models\Answer;
@@ -25,16 +26,55 @@ class AnswerAccess extends ObjectAccess
 {
 
         /**
-         * Behavour hook.
+         * Behaviour hook.
          * @param string $event
          * @param Answer $model
          * @param User $user The peer object.
-         * @param User $user The peer object.
+         * @return boolean 
+         * @throws Exception
          */
         public function notify($event, $model, $user)
         {
-                printf("%s: event=%s, model=%s, user=%s\n", __METHOD__, $event, $model->getName(),$user->getPrincipalName());
+                printf("%s: event=%s, model=%s, user=%s\n", __METHOD__, $event, $model->getName(), $user->getPrincipalName());
 
+                $role = $user->getPrimaryRole();
+
+                // 
+                // Object access control:
+                // 
+                if ($model->id != 0) {
+                        if ($role == Roles::STUDENT) {
+                                if ($model->Student->user != $user->getPrincipalName()) {
+                                        throw new Exception('owner');
+                                }
+                        }
+                }
+
+                // 
+                // Check role on exam or question:
+                // 
+                if ($role == Roles::CONTRIBUTOR ||
+                    $role == Roles::CREATOR ||
+                    $role == Roles::DECODER ||
+                    $role == Roles::INVIGILATOR) {
+                        if ($user->roles->aquire($role, $model->question->exam_id) == false) {
+                                throw new Exception('role');
+                        }
+                } elseif ($role == Roles::STUDENT) {
+                        if ($user->roles->aquire($role, $model->student->exam_id) == false) {
+                                throw new Exception('role');
+                        }
+                } elseif ($role == Roles::CORRECTOR) {
+                        if ($user->roles->aquire($role, $model->question->id) == false) {
+                                throw new Exception('role');
+                        }
+                } elseif (isset($role)) {
+                        if ($user->roles->aquire($role) == false) {
+                                throw new Exception('role');
+                        }
+                }
+
+                return true;
         }
 
 }
