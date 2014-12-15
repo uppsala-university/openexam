87f69fe77d280eff2027f4f342eb08488289edc1
Anders Lövgren <anders.lovgren@bmc.uu.se>
2014-12-15 07:08:28 +0000

    Database schema version 2.0.7: Added access table. Support for exam schedule per student and 'lämna in' (finished datetime). Fixed missing student_id in locks, adding status (pending or approved). Support for bläddertenta using questions.answer.
    git-svn-id: svn+ssh://svn.bmc.uu.se/cbe/openexam-php/branches/phalcon-mvc@1991 58b8aac4-d633-40de-91bf-eab09b0297ab

---
+++ phalcon-mvc/schemas/migrations/2.0.7/access.php
@@ -0,0 +1,75 @@
+<?php 
+
+use Phalcon\Db\Column;
+use Phalcon\Db\Index;
+use Phalcon\Db\Reference;
+use Phalcon\Mvc\Model\Migration;
+
+class AccessMigration_207 extends Migration
+{
+
+    public function up()
+    {
+        $this->morphTable(
+            'access',
+            array(
+            'columns' => array(
+                new Column(
+                    'id',
+                    array(
+                        'type' => Column::TYPE_INTEGER,
+                        'notNull' => true,
+                        'autoIncrement' => true,
+                        'size' => 11,
+                        'first' => true
+                    )
+                ),
+                new Column(
+                    'exam_id',
+                    array(
+                        'type' => Column::TYPE_INTEGER,
+                        'notNull' => true,
+                        'size' => 11,
+                        'after' => 'id'
+                    )
+                ),
+                new Column(
+                    'name',
+                    array(
+                        'type' => Column::TYPE_VARCHAR,
+                        'notNull' => true,
+                        'size' => 40,
+                        'after' => 'exam_id'
+                    )
+                ),
+                new Column(
+                    'addr',
+                    array(
+                        'type' => Column::TYPE_VARCHAR,
+                        'size' => 46,
+                        'after' => 'name'
+                    )
+                )
+            ),
+            'indexes' => array(
+                new Index('PRIMARY', array('id')),
+                new Index('exam_id', array('exam_id'))
+            ),
+            'references' => array(
+                new Reference('access_ibfk_1', array(
+                    'referencedSchema' => 'openexam',
+                    'referencedTable' => 'exams',
+                    'columns' => array('exam_id'),
+                    'referencedColumns' => array('id')
+                ))
+            ),
+            'options' => array(
+                'TABLE_TYPE' => 'BASE TABLE',
+                'AUTO_INCREMENT' => '1',
+                'ENGINE' => 'InnoDB',
+                'TABLE_COLLATION' => 'utf8_general_ci'
+            )
+        )
+        );
+    }
+}
---
+++ phalcon-mvc/schemas/migrations/2.0.7/settings.php
@@ -0,0 +1,58 @@
+<?php 
+
+use Phalcon\Db\Column;
+use Phalcon\Db\Index;
+use Phalcon\Db\Reference;
+use Phalcon\Mvc\Model\Migration;
+
+class SettingsMigration_207 extends Migration
+{
+
+    public function up()
+    {
+        $this->morphTable(
+            'settings',
+            array(
+            'columns' => array(
+                new Column(
+                    'id',
+                    array(
+                        'type' => Column::TYPE_INTEGER,
+                        'notNull' => true,
+                        'autoIncrement' => true,
+                        'size' => 11,
+                        'first' => true
+                    )
+                ),
+                new Column(
+                    'user',
+                    array(
+                        'type' => Column::TYPE_VARCHAR,
+                        'notNull' => true,
+                        'size' => 60,
+                        'after' => 'id'
+                    )
+                ),
+                new Column(
+                    'data',
+                    array(
+                        'type' => Column::TYPE_TEXT,
+                        'notNull' => true,
+                        'size' => 1,
+                        'after' => 'user'
+                    )
+                )
+            ),
+            'indexes' => array(
+                new Index('PRIMARY', array('id'))
+            ),
+            'options' => array(
+                'TABLE_TYPE' => 'BASE TABLE',
+                'AUTO_INCREMENT' => '1',
+                'ENGINE' => 'MyISAM',
+                'TABLE_COLLATION' => 'utf8_general_ci'
+            )
+        )
+        );
+    }
+}
