ed8c3edba201f0e006373d12d2e98486a767f5f1
Anders Lövgren <anders.lovgren@bmc.uu.se>
2015-03-30 03:11:57 +0000

    Adding missing foreign key between exams <-> topics.
    git-svn-id: svn+ssh://svn.bmc.uu.se/cbe/openexam-php/trunk/version2@2136 58b8aac4-d633-40de-91bf-eab09b0297ab

--- admin/sql/openexam.sql
+++ admin/sql/openexam.sql
@@ -407,7 +407,8 @@ CREATE TABLE `topics` (
   `randomize` int(11) NOT NULL DEFAULT '0',
   `grades` varchar(500) DEFAULT NULL,
   `depend` varchar(500) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  CONSTRAINT `topics_ibfk_1` FOREIGN KEY (`exam_id`) REFERENCES `exams` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
