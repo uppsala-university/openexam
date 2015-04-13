7cac492e1324a30aca4199252dd7e275b58c3ca0
Anders Lövgren <anders.lovgren@bmc.uu.se>
2015-04-13 21:04:12 +0000

    Updating database schema for modular lockdown and multiple network locations. Also fix for missing topics foreign key.
    git-svn-id: svn+ssh://svn.bmc.uu.se/cbe/openexam-php/trunk/version2@2168 58b8aac4-d633-40de-91bf-eab09b0297ab

---
+++ phalcon-mvc/schemas/migrations/2.0.9/exams.php
@@ -0,0 +1,178 @@
+<?php 
+
+use Phalcon\Db\Column;
+use Phalcon\Db\Index;
+use Phalcon\Db\Reference;
+use Phalcon\Mvc\Model\Migration;
+
+class ExamsMigration_209 extends Migration
+{
+
+    public function up()
+    {
+        $this->morphTable(
+            'exams',
+            array(
+            'columns' => array(
+                new Column(
+                    'id',
+                    array(
+                        'type' => Column::TYPE_INTEGER,
+                        'notNull' => true,
+                        'autoIncrement' => true,
+                        'size' => 11,
+                        'first' => true
+                    )
+                ),
+                new Column(
+                    'name',
+                    array(
+                        'type' => Column::TYPE_VARCHAR,
+                        'notNull' => true,
+                        'size' => 200,
+                        'after' => 'id'
+                    )
+                ),
+                new Column(
+                    'descr',
+                    array(
+                        'type' => Column::TYPE_TEXT,
+                        'size' => 1,
+                        'after' => 'name'
+                    )
+                ),
+                new Column(
+                    'starttime',
+                    array(
+                        'type' => Column::TYPE_DATETIME,
+                        'size' => 1,
+                        'after' => 'descr'
+                    )
+                ),
+                new Column(
+                    'endtime',
+                    array(
+                        'type' => Column::TYPE_DATETIME,
+                        'size' => 1,
+                        'after' => 'starttime'
+                    )
+                ),
+                new Column(
+                    'created',
+                    array(
+                        'type' => Column::TYPE_DATETIME,
+                        'notNull' => true,
+                        'size' => 1,
+                        'after' => 'endtime'
+                    )
+                ),
+                new Column(
+                    'updated',
+                    array(
+                        'type' => Column::TYPE_DATE,
+                        'notNull' => true,
+                        'size' => 1,
+                        'after' => 'created'
+                    )
+                ),
+                new Column(
+                    'creator',
+                    array(
+                        'type' => Column::TYPE_VARCHAR,
+                        'notNull' => true,
+                        'size' => 60,
+                        'after' => 'updated'
+                    )
+                ),
+                new Column(
+                    'details',
+                    array(
+                        'type' => Column::TYPE_INTEGER,
+                        'notNull' => true,
+                        'size' => 11,
+                        'after' => 'creator'
+                    )
+                ),
+                new Column(
+                    'decoded',
+                    array(
+                        'type' => Column::TYPE_CHAR,
+                        'notNull' => true,
+                        'size' => 1,
+                        'after' => 'details'
+                    )
+                ),
+                new Column(
+                    'published',
+                    array(
+                        'type' => Column::TYPE_CHAR,
+                        'size' => 1,
+                        'after' => 'decoded'
+                    )
+                ),
+                new Column(
+                    'orgunit',
+                    array(
+                        'type' => Column::TYPE_VARCHAR,
+                        'notNull' => true,
+                        'size' => 150,
+                        'after' => 'published'
+                    )
+                ),
+                new Column(
+                    'grades',
+                    array(
+                        'type' => Column::TYPE_VARCHAR,
+                        'notNull' => true,
+                        'size' => 200,
+                        'after' => 'orgunit'
+                    )
+                ),
+                new Column(
+                    'course',
+                    array(
+                        'type' => Column::TYPE_VARCHAR,
+                        'size' => 20,
+                        'after' => 'grades'
+                    )
+                ),
+                new Column(
+                    'code',
+                    array(
+                        'type' => Column::TYPE_VARCHAR,
+                        'size' => 20,
+                        'after' => 'course'
+                    )
+                ),
+                new Column(
+                    'testcase',
+                    array(
+                        'type' => Column::TYPE_CHAR,
+                        'notNull' => true,
+                        'size' => 1,
+                        'after' => 'code'
+                    )
+                ),
+                new Column(
+                    'lockdown',
+                    array(
+                        'type' => Column::TYPE_VARCHAR,
+                        'notNull' => true,
+                        'size' => 500,
+                        'after' => 'testcase'
+                    )
+                )
+            ),
+            'indexes' => array(
+                new Index('PRIMARY', array('id'))
+            ),
+            'options' => array(
+                'TABLE_TYPE' => 'BASE TABLE',
+                'AUTO_INCREMENT' => '680',
+                'ENGINE' => 'InnoDB',
+                'TABLE_COLLATION' => 'utf8_general_ci'
+            )
+        )
+        );
+    }
+}
