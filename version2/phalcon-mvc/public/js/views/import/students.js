7d6e8749381909d72cb731e643db5251fa82c512
Anders Lövgren <anders.lovgren@bmc.uu.se>
2015-04-22 05:54:51 +0000

    Adding support for import of students from file.
    git-svn-id: svn+ssh://svn.bmc.uu.se/cbe/openexam-php/trunk@2187 58b8aac4-d633-40de-91bf-eab09b0297ab

---
+++ version2/phalcon-mvc/public/js/views/import/students.js
@@ -0,0 +1,39 @@
+
+// 
+// The source code is copyrighted, with equal shared rights, between the
+// authors (see the file AUTHORS) and the OpenExam project, Uppsala University 
+// unless otherwise explicit stated elsewhere.
+// 
+// File:    students.js
+// Created: 2015-04-15 03:55:24
+// 
+// Author:  Anders Lövgren (QNET/BMC CompDept)
+// 
+
+$(document).ready(function () {
+
+    // 
+    // Handle column select actions:
+    // 
+    $(document).on('click', 'ul.dropdown-menu > li > a', function () {
+        if ($(this).attr('value') === 'remove') {
+            var nth = $(this).closest('th').index() + 1;
+            $('td:nth-child(' + nth + '),th:nth-child(' + nth + ')').hide('fast', function () {
+                $(this).remove();
+            });
+        } else {
+            $(this).closest('th').attr('value', $(this).attr('value'));
+            $(this).parents(".btn-group").find('.btn').text($(this).text());
+        }
+    });
+
+    // 
+    // Handle remove student entries:
+    //
+    $(document).on('click', 'a.import-student-remove', function () {
+        $(this).closest('tr').hide('slow', function () {
+            $(this).remove();
+        });
+    });
+
+});
