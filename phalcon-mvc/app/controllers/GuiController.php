5946070746bb67cca8d79b3cbbd959adcda86ab1
Anders Lövgren <anders.lovgren@bmc.uu.se>
2015-04-23 14:43:24 +0000

    Doing structured access control with logging in GUI controller. Bypass result -> download in access.def for now.
    git-svn-id: svn+ssh://svn.bmc.uu.se/cbe/openexam-php/trunk/version2@2192 58b8aac4-d633-40de-91bf-eab09b0297ab

--- phalcon-mvc/app/controllers/GuiController.php
+++ phalcon-mvc/app/controllers/GuiController.php
@@ -13,7 +13,11 @@
 
 namespace OpenExam\Controllers;
 
+use OpenExam\Library\Core\Error;
+use OpenExam\Library\Security\Exception as SecurityException;
+use OpenExam\Library\Security\Roles;
 use Phalcon\Config;
+use Phalcon\Logger;
 use Phalcon\Mvc\Dispatcher;
 use Phalcon\Mvc\Dispatcher\Exception as DispatcherException;
 
@@ -32,45 +36,19 @@ class GuiController extends ControllerBase
         {
                 parent::initialize();
 
-                $controller = $this->dispatcher->getControllerName();
-                $action = $this->dispatcher->getActionName();
-
-                $access = new Config(require CONFIG_DIR . '/access.def');
-
-                // if it is a private page
-                if ($controller != 'result' && $action != 'download' && $access->private->$controller->$action && count($access->private->$controller->$action->toArray())) {
-
-                        $allowedRoles = $access->private->$controller->$action->toArray();
-
-                        # if this page is not accessable for all logged in persons, 
-                        # check if logged in person has appropriate role to access
-                        # this page
-                        if (!array_search('*', $allowedRoles)) {
-
-                                if (($examId = $_REQUEST['exam_id']) || ($examId = $_REQUEST['examId']) || ($examId = $this->dispatcher->getParam("examId"))) {
-
-                                        $aquired = $this->user->aquire($allowedRoles, $examId);
-
-                                        if (
-                                            !$aquired &&
-                                            array_search('corrector', $allowedRoles) &&
-                                            (($qId = $_REQUEST['q_id']) || ($qId = $_REQUEST['questionId']) || ($qId = $this->dispatcher->getParam("questId")))
-                                        ) {
-                                                $aquired = $this->user->aquire(array('corrector'), $qId);
-                                        }
-                                } else {
-                                        $aquired = $this->user->aquire($allowedRoles);
-                                }
-
-                                if (!$aquired) {
-                                        die("Appologies! you don't have permissions to access this URL.<br> "
-                                            . "Please <a href='mailto:ahsan.shahzad@medfarm.uu.se'>contact us</a> in case you should be allowed.");
-                                }
-                        }
+                // 
+                // Normal exception handling is not working at dispatch time.
+                // We need to use try/catch with forward to error page.
+                // 
+                try {
+                        $this->checkAccess(
+                            $this->dispatcher->getControllerName(), $this->dispatcher->getActionName()
+                        );
+                        $this->view->setVar("authenticators", $this->auth->getChain("web"));
+                        $this->view->setLayout('main');
+                } catch (\Exception $exception) {
+                        return $this->exceptionAction($exception);
                 }
-
-                $this->view->setVar("authenticators", $this->auth->getChain("web"));
-                $this->view->setLayout('main');
         }
 
         /**
@@ -92,15 +70,15 @@ class GuiController extends ControllerBase
                                         return false;
                         }
                 }
-                
+
                 if ($exception->getCode() == 0) {
-                        $this->report($exception);
                         $this->dispatcher->forward(array(
                                 'controller' => 'error',
                                 'action'     => 'show503',
                                 'namespace'  => 'OpenExam\Controllers\Gui',
                                 'params'     => array('exception' => $exception)
                         ));
+                        $this->report($exception);
                         return false;
                 } else {
                         $this->report($exception);
@@ -112,6 +90,104 @@ class GuiController extends ControllerBase
                         ));
                         return false;
                 }
+        }
+
+        /**
+         * Perform access check.
+         * 
+         * Check if caller has permission to access URL (controller -> action)
+         * based on rules in user configuration file access.def. The control
+         * is done against list of explicit defined private URL's. If a URL
+         * is not defined, then access is permitted.
+         * 
+         * Object specific check (thru role aquire) is done if exam or question 
+         * ID is passed in request. The order of access check is: question,
+         * exam and then generic role aquire check.
+         * 
+         * Return true if access is permitted. Throws exception if URL is 
+         * private and none accepted role was aquired.
+         * 
+         * @param string $controller The target controller.
+         * @param string $action The target action.
+         * @return boolean
+         * @throws SecurityException
+         */
+        private function checkAccess($controller, $action)
+        {
+                $access = new Config(require CONFIG_DIR . '/access.def');
+                $permit = $access->private->$controller->$action;
+                
+                // 
+                // Bypass for non-configured controller/action:
+                // 
+                if (!isset($permit)) {
+                        if ($this->logger->auth->getLogLevel() >= Logger::DEBUG) {
+                                $this->logger->auth->debug(sprintf("Bypass access restriction for %s on %s -> %s (access rule missing)", $this->user->getPrincipalName(), $controller, $action));
+                        }
+                        return true;
+                }
+                if (count($permit->toArray()) == 0) {
+                        if ($this->logger->auth->getLogLevel() >= Logger::DEBUG) {
+                                $this->logger->auth->debug(sprintf("Bypass access restriction for %s on %s -> %s (access rules empty)", $this->user->getPrincipalName(), $controller, $action));
+                        }
+                        return true;
+                }
+
+                // 
+                // Using roles == '*' means access for all roles:
+                // 
+                if ($permit[0] == '*') {
+                        if ($this->logger->auth->getLogLevel() >= Logger::DEBUG) {
+                                $this->logger->auth->debug(sprintf("Bypass access restriction for %s on %s -> %s (access rule is '*')", $this->user->getPrincipalName(), $controller, $action));
+                        }
+                        return true;
+                }
+
+                // 
+                // Check question access:
+                // 
+                if (($id = $this->request->get('q_id', 'int')) ||
+                    ($id = $this->request->get('questionId', 'int')) ||
+                    ($id = $this->dispatcher->getParam('questId', 'int'))) {
+                        if ($this->user->roles->aquire(Roles::CORRECTOR, $id)) {
+                                if ($this->logger->auth->getLogLevel() >= Logger::DEBUG) {
+                                        $this->logger->auth->debug(sprintf("Permitted question access for %s on %s -> %s (id: %d, roles: %s)", $this->user->getPrincipalName(), $controller, $action, $id, Roles::CORRECTOR));
+                                }
+                                return true;
+                        }
+                }
+
+                // 
+                // Check exam access:
+                // 
+                if (($id = $this->request->get('exam_id', 'int')) ||
+                    ($id = $this->request->get('examId', 'int')) ||
+                    ($id = $this->dispatcher->getParam('examId', 'int'))) {
+                        if (($roles = $this->user->aquire($permit, $id))) {
+                                if ($this->logger->auth->getLogLevel() >= Logger::DEBUG) {
+                                        $this->logger->auth->debug(sprintf("Permitted exam access for %s on %s -> %s (id: %d, roles: %s)", $this->user->getPrincipalName(), $controller, $action, $id, implode(",", $roles)));
+                                }
+                                return true;
+                        }
+                }
+
+                // 
+                // Check role access:
+                // 
+                if (($roles = $this->user->aquire($permit))) {
+                        if ($this->logger->auth->getLogLevel() >= Logger::DEBUG) {
+                                $this->logger->auth->debug(sprintf("Permitted role access for %s on %s -> %s (roles: %s)", $this->user->getPrincipalName(), $controller, $action, implode(",", $roles)));
+                        }
+                        return true;
+                }
+
+                // 
+                // Nuke access with a proper contact us message:
+                // 
+                throw new SecurityException(sprintf(
+                    "You are not allowed to access this URL. Please <a href=\"mailto:%s\">%s</a> if you think this is an error.", $this->config->contact->addr, $this->config->contact->name
+                ), Error::METHOD_NOT_ALLOWED
+                );
         }
 
 }
