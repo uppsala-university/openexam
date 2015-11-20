4f779c7b21e8f91d28280bbbe5143350769372a3
Anders Lövgren <anders.lovgren@bmc.uu.se>
2015-11-20 11:08:53 +0000

    Migrations and SQL script for latest database staging.
    git-svn-id: svn+ssh://svn.bmc.uu.se/cbe/openexam-php/trunk@2230 58b8aac4-d633-40de-91bf-eab09b0297ab

---
+++ version2/phalcon-mvc/schemas/migrations/2.0.10/files.php
@@ -0,0 +1,94 @@
+<?php 
+
+use Phalcon\Db\Column;
+use Phalcon\Db\Index;
+use Phalcon\Db\Reference;
+use Phalcon\Mvc\Model\Migration;
+
+class FilesMigration_2010 extends Migration
+{
+
+    public function up()
+    {
+        $this->morphTable(
+            'files',
+            array(
+            'columns' => array(
+                new Column(
+                    'id',
+                    array(
+                        'type' => Column::TYPE_INTEGER,
+                        'notNull' => true,
+                        'autoIncrement' => true,
+                        'size' => 11,
+                        'first' => true
+                    )
+                ),
+                new Column(
+                    'answer_id',
+                    array(
+                        'type' => Column::TYPE_INTEGER,
+                        'notNull' => true,
+                        'size' => 11,
+                        'after' => 'id'
+                    )
+                ),
+                new Column(
+                    'name',
+                    array(
+                        'type' => Column::TYPE_VARCHAR,
+                        'notNull' => true,
+                        'size' => 50,
+                        'after' => 'answer_id'
+                    )
+                ),
+                new Column(
+                    'path',
+                    array(
+                        'type' => Column::TYPE_VARCHAR,
+                        'notNull' => true,
+                        'size' => 120,
+                        'after' => 'name'
+                    )
+                ),
+                new Column(
+                    'type',
+                    array(
+                        'type' => Column::TYPE_VARCHAR,
+                        'notNull' => true,
+                        'size' => 25,
+                        'after' => 'path'
+                    )
+                ),
+                new Column(
+                    'subtype',
+                    array(
+                        'type' => Column::TYPE_VARCHAR,
+                        'notNull' => true,
+                        'size' => 25,
+                        'after' => 'type'
+                    )
+                )
+            ),
+            'indexes' => array(
+                new Index('PRIMARY', array('id')),
+                new Index('answer_id', array('answer_id'))
+            ),
+            'references' => array(
+                new Reference('files_ibfk_1', array(
+                    'referencedSchema' => 'openexam2prod',
+                    'referencedTable' => 'answers',
+                    'columns' => array('answer_id'),
+                    'referencedColumns' => array('id')
+                ))
+            ),
+            'options' => array(
+                'TABLE_TYPE' => 'BASE TABLE',
+                'AUTO_INCREMENT' => '1',
+                'ENGINE' => 'InnoDB',
+                'TABLE_COLLATION' => 'utf8_general_ci'
+            )
+        )
+        );
+    }
+}
