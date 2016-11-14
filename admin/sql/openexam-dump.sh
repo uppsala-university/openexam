2ade474a4d7f274aa973513d57b1e78c22af3a93
Anders Lövgren <anders.lovgren@bmc.uu.se>
2016-11-14 05:14:46 +0000

    Restored MySQL dump script.
    git-svn-id: svn+ssh://svn.bmc.uu.se/cbe/openexam-php/trunk@2550 58b8aac4-d633-40de-91bf-eab09b0297ab

--- admin/sql/openexam-dump.sh
+++ admin/sql/openexam-dump.sh
@@ -6,5 +6,5 @@
 # Date:   2010-05-17
 #
 
-mysqldump -u root -p -d openexam2prod | sed s%'AUTO_INCREMENT=\([0-9]*\)'%'AUTO_INCREMENT=1'%g > openexam.sql
-mysqldump -u root -p -d -X openexam2prod | sed s%'Auto_increment="\([0-9]*\)"'%'Auto_increment="1"'%g > openexam.xml
+mysqldump -u root -p -d    openexam2 | sed s%'AUTO_INCREMENT=\([0-9]*\)'%'AUTO_INCREMENT=1'%g     > openexam.sql
+mysqldump -u root -p -d -X openexam2 | sed s%'Auto_increment="\([0-9]*\)"'%'Auto_increment="1"'%g > openexam.xml
