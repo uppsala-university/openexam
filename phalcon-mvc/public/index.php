781a2d541be6d440822f4a656ee6b0e022b2a576
Anders Lövgren <anders.lovgren@bmc.uu.se>
2016-12-01 03:50:24 +0000

    Keep system configuration in cache. This saves ~20% of execution time on start page.
    git-svn-id: svn+ssh://svn.bmc.uu.se/cbe/openexam-php/trunk@2628 58b8aac4-d633-40de-91bf-eab09b0297ab

--- phalcon-mvc/public/index.php
+++ phalcon-mvc/public/index.php
@@ -1,12 +1,55 @@
 <?php
 
 /**
+ * System configuration directory.
+ */
+define('CONFIG_SYS', realpath(__DIR__ . '/../app/config/system'));
+
+/**
+ * User settings directory.
+ */
+define('CONFIG_DIR', dirname(CONFIG_SYS));
+
+/**
+ * Application directory.
+ */
+define('APP_DIR', dirname(CONFIG_DIR));
+
+/**
+ * Base directory (the Phalcon MVC app).
+ */
+define('BASE_DIR', dirname(APP_DIR));
+
+/**
+ * Project root directory.
+ */
+define('PROJ_DIR', dirname(BASE_DIR));
+
+/**
+ * External source directory.
+ */
+define('EXTERN_DIR', APP_DIR . '/extern/');
+
+/**
  * The bootstrap script called by Phalcon.
  */
+define('CONFIG_PHP', CONFIG_SYS . '/config.php');
 
-define('CONFIG_PHP', __DIR__ . '/../app/config/system/config.php');
-
-$config = include(CONFIG_PHP);
+// 
+// Keep system configuration in cache:
+// 
+$frontend = new Phalcon\Cache\Frontend\Data(array(
+        "lifetime" => 3600
+    )
+);
+$backend = new Phalcon\Cache\Backend\Xcache($frontend, array(
+        "prefix" => "config-data"
+    )
+);
+if (!($config = $backend->get('site-config'))) {
+        $config = include(CONFIG_PHP);
+        $cache->save('site-config', $config);
+}
 
 if ($config->application->release) {
         error_reporting(E_ALL ^ E_NOTICE & ~E_DEPRECATED);
