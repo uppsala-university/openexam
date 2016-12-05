78a270055b1ec332f9f0f267f35ef1783828783a
Anders Lövgren <anders.lovgren@bmc.uu.se>
2016-12-05 04:48:03 +0000

    Fixed a problem in core error class when exception code is numeric, but don't map to any HTTP status code. Typical for database has gone. This prevented correct error page from being rendered as it was forged by error page on non-standard HTTP code in response.
    git-svn-id: svn+ssh://svn.bmc.uu.se/cbe/openexam-php/trunk@2647 58b8aac4-d633-40de-91bf-eab09b0297ab

--- phalcon-mvc/app/library/Core/Error.php
+++ phalcon-mvc/app/library/Core/Error.php
@@ -83,6 +83,84 @@ class Error
          * @var int 
          */
         private $_status;
+        /**
+         * HTTP errors
+         * @var array 
+         */
+        private static $http_codes = array(
+                // 
+                // Error code unset:
+                // 
+                0   => 'Unknown Error',
+                // 
+                // Security exception extensions:
+                // 
+                1   => 'Failed Acquire Role',
+                2   => 'Not Object Owner',
+                3   => 'Access Denied',
+                4   => 'Action Not Allowed',
+                5   => 'ACL Service Missing',
+                6   => 'User Service Missing',
+                7   => 'Not Authenticated',
+                // 
+                // Standard HTTP codes:
+                // 
+                100 => 'Continue',
+                101 => 'Switching Protocols',
+                102 => 'Processing',
+                200 => 'OK',
+                201 => 'Created',
+                202 => 'Accepted',
+                203 => 'Non-Authoritative Information',
+                204 => 'No Content',
+                205 => 'Reset Content',
+                206 => 'Partial Content',
+                207 => 'Multi-Status',
+                300 => 'Multiple Choices',
+                301 => 'Moved Permanently',
+                302 => 'Found',
+                303 => 'See Other',
+                304 => 'Not Modified',
+                305 => 'Use Proxy',
+                306 => 'Switch Proxy',
+                307 => 'Temporary Redirect',
+                400 => 'Bad Request',
+                401 => 'Unauthorized',
+                402 => 'Payment Required',
+                403 => 'Forbidden',
+                404 => 'Not Found',
+                405 => 'Method Not Allowed',
+                406 => 'Not Acceptable',
+                407 => 'Proxy Authentication Required',
+                408 => 'Request Timeout',
+                409 => 'Conflict',
+                410 => 'Gone',
+                411 => 'Length Required',
+                412 => 'Precondition Failed',
+                413 => 'Request Entity Too Large',
+                414 => 'Request-URI Too Long',
+                415 => 'Unsupported Media Type',
+                416 => 'Requested Range Not Satisfiable',
+                417 => 'Expectation Failed',
+                418 => 'I\'m a teapot',
+                422 => 'Unprocessable Entity',
+                423 => 'Locked',
+                424 => 'Failed Dependency',
+                425 => 'Unordered Collection',
+                426 => 'Upgrade Required',
+                449 => 'Retry With',
+                450 => 'Blocked by Windows Parental Controls',
+                500 => 'Internal Server Error',
+                501 => 'Not Implemented',
+                502 => 'Bad Gateway',
+                503 => 'Service Unavailable',
+                504 => 'Gateway Timeout',
+                505 => 'HTTP Version Not Supported',
+                506 => 'Variant Also Negotiates',
+                507 => 'Insufficient Storage',
+                509 => 'Bandwidth Limit Exceeded',
+                510 => 'Not Extended'
+        );
 
         /**
          * Constructor
@@ -91,10 +169,11 @@ class Error
         public function __construct($exception)
         {
                 $this->_exception = $exception;
+                $this->_status = $exception->getCode();
 
-                if (is_numeric($this->_exception->getCode())) {
-                        $this->_status = $this->_exception->getCode();
-                } else {
+                if (!is_numeric($this->_status)) {
+                        $this->_status = self::INTERNAL_SERVER_ERROR;
+                } elseif (!array_key_exists($this->_status, self::$http_codes)) {
                         $this->_status = self::INTERNAL_SERVER_ERROR;
                 }
         }
@@ -132,7 +211,7 @@ class Error
          */
         public function getString()
         {
-                return self::lookup($this->_exception->getCode());
+                return self::lookup($this->_status);
         }
 
         /**
@@ -152,81 +231,7 @@ class Error
          */
         public static function lookup($code)
         {
-                static $http_codes = array(
-                        // 
-                        // Error code unset:
-                        // 
-                        0   => 'Unknown Error',
-                        // 
-                        // Security exception extensions:
-                        // 
-                        1   => 'Failed Acquire Role',
-                        2   => 'Not Object Owner',
-                        3   => 'Access Denied',
-                        4   => 'Action Not Allowed',
-                        5   => 'ACL Service Missing',
-                        6   => 'User Service Missing',
-                        7   => 'Not Authenticated',
-                        // 
-                        // Standard HTTP codes:
-                        // 
-                        100 => 'Continue',
-                        101 => 'Switching Protocols',
-                        102 => 'Processing',
-                        200 => 'OK',
-                        201 => 'Created',
-                        202 => 'Accepted',
-                        203 => 'Non-Authoritative Information',
-                        204 => 'No Content',
-                        205 => 'Reset Content',
-                        206 => 'Partial Content',
-                        207 => 'Multi-Status',
-                        300 => 'Multiple Choices',
-                        301 => 'Moved Permanently',
-                        302 => 'Found',
-                        303 => 'See Other',
-                        304 => 'Not Modified',
-                        305 => 'Use Proxy',
-                        306 => 'Switch Proxy',
-                        307 => 'Temporary Redirect',
-                        400 => 'Bad Request',
-                        401 => 'Unauthorized',
-                        402 => 'Payment Required',
-                        403 => 'Forbidden',
-                        404 => 'Not Found',
-                        405 => 'Method Not Allowed',
-                        406 => 'Not Acceptable',
-                        407 => 'Proxy Authentication Required',
-                        408 => 'Request Timeout',
-                        409 => 'Conflict',
-                        410 => 'Gone',
-                        411 => 'Length Required',
-                        412 => 'Precondition Failed',
-                        413 => 'Request Entity Too Large',
-                        414 => 'Request-URI Too Long',
-                        415 => 'Unsupported Media Type',
-                        416 => 'Requested Range Not Satisfiable',
-                        417 => 'Expectation Failed',
-                        418 => 'I\'m a teapot',
-                        422 => 'Unprocessable Entity',
-                        423 => 'Locked',
-                        424 => 'Failed Dependency',
-                        425 => 'Unordered Collection',
-                        426 => 'Upgrade Required',
-                        449 => 'Retry With',
-                        450 => 'Blocked by Windows Parental Controls',
-                        500 => 'Internal Server Error',
-                        501 => 'Not Implemented',
-                        502 => 'Bad Gateway',
-                        503 => 'Service Unavailable',
-                        504 => 'Gateway Timeout',
-                        505 => 'HTTP Version Not Supported',
-                        506 => 'Variant Also Negotiates',
-                        507 => 'Insufficient Storage',
-                        509 => 'Bandwidth Limit Exceeded',
-                        510 => 'Not Extended'
-                );
-                return $http_codes[$code];
+                return self::$http_codes[$code];
         }
 
 }
