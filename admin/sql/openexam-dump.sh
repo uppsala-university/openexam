4194f26893b02622445efdbc5dcead0dde248319
Anders Lövgren <anders.lovgren@bmc.uu.se>
2016-12-13 18:33:06 +0000

    Script for dumping MySQL database scheme is now supporting dump using '--login-path=local' (password less) or '-u root -p' (standard login).
    git-svn-id: svn+ssh://svn.bmc.uu.se/cbe/openexam-php/trunk@2673 58b8aac4-d633-40de-91bf-eab09b0297ab

--- admin/sql/openexam-dump.sh
+++ admin/sql/openexam-dump.sh
@@ -6,5 +6,29 @@
 # Date:   2010-05-17
 #
 
-mysqldump -u root -p -d    openexam2 | sed s%'AUTO_INCREMENT=\([0-9]*\)'%'AUTO_INCREMENT=1'%g     > openexam.sql
-mysqldump -u root -p -d -X openexam2 | sed s%'Auto_increment="\([0-9]*\)"'%'Auto_increment="1"'%g > openexam.xml
+function dump_local()
+{
+    mysqldump --login-path=local -d    openexam2 | sed s%'AUTO_INCREMENT=\([0-9]*\)'%'AUTO_INCREMENT=1'%g     > openexam.sql
+    mysqldump --login-path=local -d -X openexam2 | sed s%'Auto_increment="\([0-9]*\)"'%'Auto_increment="1"'%g > openexam.xml
+}
+
+function dump_login()
+{
+    mysqldump -u root -p -d    openexam2 | sed s%'AUTO_INCREMENT=\([0-9]*\)'%'AUTO_INCREMENT=1'%g     > openexam.sql
+    mysqldump -u root -p -d -X openexam2 | sed s%'Auto_increment="\([0-9]*\)"'%'Auto_increment="1"'%g > openexam.xml
+}
+
+if [ -z "$1" ]; then
+    login="local"
+else 
+    login="$1"
+fi
+
+case "$login" in
+    local)
+        dump_local
+        ;;
+    login)
+        dump_login
+        ;;
+esac
