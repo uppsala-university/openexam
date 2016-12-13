d6bc084de6db2466b86b76083b3b6249f6c6ddd2
Anders Lövgren <anders.lovgren@bmc.uu.se>
2016-12-13 14:10:32 +0000

    Adding MySQL script for dumping audit database.
    git-svn-id: svn+ssh://svn.bmc.uu.se/cbe/openexam-php/trunk@2672 58b8aac4-d633-40de-91bf-eab09b0297ab

---
+++ admin/sql/audit/openexam-dump.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+# Simple script that dumps the database scheme to SQL and XML-files.
+#
+# Author: Anders Lövgren
+# Date:   2010-05-17
+#
+
+mysqldump -u root -p -d    openexam2audit | sed s%'AUTO_INCREMENT=\([0-9]*\)'%'AUTO_INCREMENT=1'%g     > openexam.sql
+mysqldump -u root -p -d -X openexam2audit | sed s%'Auto_increment="\([0-9]*\)"'%'Auto_increment="1"'%g > openexam.xml
