679c01e6792e5cb261b4f7733ee3c155e7f47787
Anders Lövgren <anders.lovgren@bmc.uu.se>
2017-03-20 16:01:23 +0000

    Fix ordering of getPrincipal() arguments passed to catalog during export of PDF containing registered student. This broke SQL (equals) in database catalog service.
    git-svn-id: svn+ssh://svn.bmc.uu.se/cbe/openexam-php/trunk@2896 58b8aac4-d633-40de-91bf-eab09b0297ab

--- phalcon-mvc/app/controllers/Utility/ExportController.php
+++ phalcon-mvc/app/controllers/Utility/ExportController.php
@@ -84,7 +84,7 @@ class ExportController extends GuiController
                 // 
                 // Exam contact information.
                 // 
-                $contact = $this->catalog->getPrincipal(Principal::ATTR_PN, $exam->creator);
+                $contact = $this->catalog->getPrincipal($exam->creator, Principal::ATTR_PN);
 
                 $this->view->setVar('students', $students);
                 $this->view->setVar('exam', $exam);
