bf143d7dd9b5354ecdbd8307bea2b09d073316df
Anders Lövgren <ipv6nu@gmail.com>
2018-01-08 06:17:28 +0100

    Adding Phalcon 3.3.0 API docs.

---
+++ docs/api/phalcon/3.3.0/Phalcon/Mvc/View/Engine/Volt/Exception.php
@@ -0,0 +1,24 @@
+<?php 
+
+namespace Phalcon\Mvc\View\Engine\Volt {
+
+	/**
+	 * Phalcon\Mvc\View\Exception
+	 *
+	 * Class for exceptions thrown by Phalcon\Mvc\View
+	 */
+	
+	class Exception extends \Phalcon\Mvc\View\Exception {
+
+		protected $statement;
+
+		public function __construct($message=null, $statement=null, $code=null, \Exception $previous=null){ }
+
+
+		/**
+		 * Gets currently parsed statement (if any).
+		 */
+		public function getStatement(){ }
+
+	}
+}
