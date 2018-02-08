c96022911956c94c7a75c57973464d6b2d469970
Anders Lövgren <ipv6nu@gmail.com>
2018-02-08 01:35:27 +0100

    New database migration for current database.

---
+++ phalcon-mvc/schemas/migrations/2.0.16/render.php
@@ -0,0 +1,171 @@
+<?php 
+
+use Phalcon\Db\Column;
+use Phalcon\Db\Index;
+use Phalcon\Db\Reference;
+use Phalcon\Mvc\Model\Migration;
+
+/**
+ * Class RenderMigration_216
+ */
+class RenderMigration_216 extends Migration
+{
+    /**
+     * Define the table structure
+     *
+     * @return void
+     */
+    public function morph()
+    {
+        $this->morphTable('render', [
+                'columns' => [
+                    new Column(
+                        'id',
+                        [
+                            'type' => Column::TYPE_INTEGER,
+                            'notNull' => true,
+                            'autoIncrement' => true,
+                            'size' => 11,
+                            'first' => true
+                        ]
+                    ),
+                    new Column(
+                        'exam_id',
+                        [
+                            'type' => Column::TYPE_INTEGER,
+                            'notNull' => true,
+                            'size' => 11,
+                            'after' => 'id'
+                        ]
+                    ),
+                    new Column(
+                        'queued',
+                        [
+                            'type' => Column::TYPE_TIMESTAMP,
+                            'default' => "CURRENT_TIMESTAMP",
+                            'notNull' => true,
+                            'size' => 1,
+                            'after' => 'exam_id'
+                        ]
+                    ),
+                    new Column(
+                        'finish',
+                        [
+                            'type' => Column::TYPE_DATETIME,
+                            'size' => 1,
+                            'after' => 'queued'
+                        ]
+                    ),
+                    new Column(
+                        'user',
+                        [
+                            'type' => Column::TYPE_VARCHAR,
+                            'notNull' => true,
+                            'size' => 60,
+                            'after' => 'finish'
+                        ]
+                    ),
+                    new Column(
+                        'url',
+                        [
+                            'type' => Column::TYPE_VARCHAR,
+                            'notNull' => true,
+                            'size' => 300,
+                            'after' => 'user'
+                        ]
+                    ),
+                    new Column(
+                        'path',
+                        [
+                            'type' => Column::TYPE_VARCHAR,
+                            'size' => 120,
+                            'after' => 'url'
+                        ]
+                    ),
+                    new Column(
+                        'wait',
+                        [
+                            'type' => Column::TYPE_INTEGER,
+                            'default' => "0",
+                            'notNull' => true,
+                            'size' => 11,
+                            'after' => 'path'
+                        ]
+                    ),
+                    new Column(
+                        'type',
+                        [
+                            'type' => Column::TYPE_CHAR,
+                            'default' => "result",
+                            'notNull' => true,
+                            'size' => 1,
+                            'after' => 'wait'
+                        ]
+                    ),
+                    new Column(
+                        'status',
+                        [
+                            'type' => Column::TYPE_CHAR,
+                            'default' => "missing",
+                            'notNull' => true,
+                            'size' => 1,
+                            'after' => 'type'
+                        ]
+                    ),
+                    new Column(
+                        'message',
+                        [
+                            'type' => Column::TYPE_VARCHAR,
+                            'size' => 100,
+                            'after' => 'status'
+                        ]
+                    )
+                ],
+                'indexes' => [
+                    new Index('PRIMARY', ['id'], 'PRIMARY'),
+                    new Index('exam_id', ['exam_id'], null)
+                ],
+                'references' => [
+                    new Reference(
+                        'render_ibfk_1',
+                        [
+                            'referencedTable' => 'exams',
+                            'referencedSchema' => 'openexam2prod',
+                            'columns' => ['exam_id'],
+                            'referencedColumns' => ['id'],
+                            'onUpdate' => 'RESTRICT',
+                            'onDelete' => 'CASCADE'
+                        ]
+                    )
+                ],
+                'options' => [
+                    'TABLE_TYPE' => 'BASE TABLE',
+                    'AUTO_INCREMENT' => '3244',
+                    'ENGINE' => 'InnoDB',
+                    'TABLE_COLLATION' => 'utf8_general_ci'
+                ],
+            ]
+        );
+    }
+
+    /**
+     * Run the migrations
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+    }
+
+    /**
+     * Reverse the migrations
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+    }
+
+}
---
+++ phalcon-mvc/schemas/migrations/2.0.16/results.php
@@ -0,0 +1,158 @@
+<?php 
+
+use Phalcon\Db\Column;
+use Phalcon\Db\Index;
+use Phalcon\Db\Reference;
+use Phalcon\Mvc\Model\Migration;
+
+/**
+ * Class ResultsMigration_216
+ */
+class ResultsMigration_216 extends Migration
+{
+    /**
+     * Define the table structure
+     *
+     * @return void
+     */
+    public function morph()
+    {
+        $this->morphTable('results', [
+                'columns' => [
+                    new Column(
+                        'id',
+                        [
+                            'type' => Column::TYPE_INTEGER,
+                            'notNull' => true,
+                            'autoIncrement' => true,
+                            'size' => 11,
+                            'first' => true
+                        ]
+                    ),
+                    new Column(
+                        'answer_id',
+                        [
+                            'type' => Column::TYPE_INTEGER,
+                            'notNull' => true,
+                            'size' => 11,
+                            'after' => 'id'
+                        ]
+                    ),
+                    new Column(
+                        'corrector_id',
+                        [
+                            'type' => Column::TYPE_INTEGER,
+                            'notNull' => true,
+                            'size' => 11,
+                            'after' => 'answer_id'
+                        ]
+                    ),
+                    new Column(
+                        'question_id',
+                        [
+                            'type' => Column::TYPE_INTEGER,
+                            'notNull' => true,
+                            'size' => 11,
+                            'after' => 'corrector_id'
+                        ]
+                    ),
+                    new Column(
+                        'correction',
+                        [
+                            'type' => Column::TYPE_CHAR,
+                            'default' => "waiting",
+                            'notNull' => true,
+                            'size' => 1,
+                            'after' => 'question_id'
+                        ]
+                    ),
+                    new Column(
+                        'score',
+                        [
+                            'type' => Column::TYPE_VARCHAR,
+                            'notNull' => true,
+                            'size' => 1024,
+                            'after' => 'correction'
+                        ]
+                    ),
+                    new Column(
+                        'comment',
+                        [
+                            'type' => Column::TYPE_TEXT,
+                            'size' => 1,
+                            'after' => 'score'
+                        ]
+                    )
+                ],
+                'indexes' => [
+                    new Index('PRIMARY', ['id'], 'PRIMARY'),
+                    new Index('answer_id', ['answer_id'], null),
+                    new Index('corrector_id', ['corrector_id'], null),
+                    new Index('question_id', ['question_id'], null)
+                ],
+                'references' => [
+                    new Reference(
+                        'results_ibfk_1',
+                        [
+                            'referencedTable' => 'answers',
+                            'referencedSchema' => 'openexam2prod',
+                            'columns' => ['answer_id'],
+                            'referencedColumns' => ['id'],
+                            'onUpdate' => 'RESTRICT',
+                            'onDelete' => 'CASCADE'
+                        ]
+                    ),
+                    new Reference(
+                        'results_ibfk_2',
+                        [
+                            'referencedTable' => 'correctors',
+                            'referencedSchema' => 'openexam2prod',
+                            'columns' => ['corrector_id'],
+                            'referencedColumns' => ['id'],
+                            'onUpdate' => 'RESTRICT',
+                            'onDelete' => 'CASCADE'
+                        ]
+                    ),
+                    new Reference(
+                        'results_ibfk_3',
+                        [
+                            'referencedTable' => 'questions',
+                            'referencedSchema' => 'openexam2prod',
+                            'columns' => ['question_id'],
+                            'referencedColumns' => ['id'],
+                            'onUpdate' => 'RESTRICT',
+                            'onDelete' => 'CASCADE'
+                        ]
+                    )
+                ],
+                'options' => [
+                    'TABLE_TYPE' => 'BASE TABLE',
+                    'AUTO_INCREMENT' => '292830',
+                    'ENGINE' => 'InnoDB',
+                    'TABLE_COLLATION' => 'utf8_general_ci'
+                ],
+            ]
+        );
+    }
+
+    /**
+     * Run the migrations
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+    }
+
+    /**
+     * Reverse the migrations
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+    }
+
+}
