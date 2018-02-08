f079ec1c2cc90497593bfc42b5e528c7c6bab32a
Anders Lövgren <ipv6nu@gmail.com>
2018-02-08 02:46:25 +0100

    Send HTTP 405 if calling fragment render isn't allowed.

--- phalcon-mvc/app/controllers/Utility/RenderController.php
+++ phalcon-mvc/app/controllers/Utility/RenderController.php
@@ -607,12 +607,12 @@ class RenderController extends GuiController
                 if ($this->config->render->submit) {
                         if (is_string($this->config->render->submit)) {
                                 if ($this->request->getClientAddress() != $this->config->render->submit) {
-                                        throw new Exception("You are not permitted to submit render job");
+                                        throw new Exception("You are not permitted to submit render job", Error::METHOD_NOT_ALLOWED);
                                 }
                         }
                         if (is_array($this->config->render->submit)) {
                                 if (!in_array($this->request->getClientAddress(), $this->config->render->submit)) {
-                                        throw new Exception("You are not permitted to submit render job");
+                                        throw new Exception("You are not permitted to submit render job", Error::METHOD_NOT_ALLOWED);
                                 }
                         }
                 }
